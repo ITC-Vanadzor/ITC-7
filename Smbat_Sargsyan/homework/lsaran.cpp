5b58e0d95cd703f001d14a314f4ee465ff8718b5
Smbat888 <sargsyanyan@mail.ru>
2015-10-25 19:31:52 +0300

    Update lsaran.cpp
    i deleted the function F,and writed it without some function

--- Smbat_Sargsyan/homework/lsaran.cpp
+++ Smbat_Sargsyan/homework/lsaran.cpp
@@ -1,36 +1,65 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int n,m,i;
-int karg,mnacord;
-int a[9];
-void F(int a[i],int m) //F funkcian hashvum e 1-ic minchev m(n) exac tvanshanneri qanaky amen mi tvanshani hamar
-{
-    do
-        k=n/10;
-        karg++;
-        k=n;
-    } while(k>0);
-    mnacord=mnacord%10;
-    for(i=1;i<mnacord;i++)
-       { a[i]=a[i]+mnacord*(karg-1)*pow(10, (karg-2))+pow(10, (karg-1)); }
-    for(i=mnacord;i<=9;i++)
-    { a[i]=a[i]+(karg-1)*pow(10, (karg-2)); }
-    return a[i];
-}
 int main()
 {
+	int n;
 	cout<<"n= ";
 	cin>>n;
+	int m;
 	cout<<"m= ";
 	cin>>m;
+	int a[9]; //zangvac vory pahum e tvanshannery(0....9)
 	for (int i=0; i<=9; ++i)
 	{
 		a[i]=0;
 	}
-	F(a[i],n);
-	F(a[i],m);
-	a[i]=F(m,a[i])-F(n,a[i]);
-	cout << a[i] << endl ;
-	retrn 0;
+int n1; //n tivy pahelu hamar
+n1=n;
+int karg;
+int m1 //m tivy pahelu hamar;
+m1=m;
+int mtiv;
+karg=1;
+do
+{
+mtiv=m1%10;
+for (int i=1; i<mtiv; ++i)
+{
+a[i]+= mtiv*(karg-1)*pow(10, (karg-2))+pow(10, (karg-1));
+}
+for (int i=mtiv-1; i>0,i<=9; ++i)
+{
+a[i]+= mtiv*(karg-1)*pow(10, (karg-2));
+}
+a[mtiv]+=mtiv*(karg-1)*pow(10, (karg-2))+1;
+m1/=10;
+++karg;
+}
+while (m1>0);
+karg=1;
+int ntiv;
+do
+{
+ntiv=n1y%10;
+for (int i=1; i<ntiv; ++i)
+{
+a[i]-= ntiv*(karg-1)*pow(10, (karg-2))+pow(10, (karg-1));
+}
+for (int i=ntiv-1; i>0,i<=9; ++i)
+{
+a[i]-= ntiv*(karg-1)*pow(10, (karg-2));
+}
+a[ntiv]-=ntiv*(karg-1)*pow(10, (karg-2));
+n1/=10;
+++karg;
+}
+while (n1>0);
+
+for (int i=0; i<=9; ++i)
+	{
+		cout<<tvanshan[i]<<'\t';
+	}
+
+	return 0;
 }

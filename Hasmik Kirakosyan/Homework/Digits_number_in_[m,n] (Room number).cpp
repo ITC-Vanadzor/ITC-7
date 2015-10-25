4a3f85758d31d70b056526216bae19c11a7066e4
Hasmikus <hasmikus@gmail.com>
2015-10-25 23:59:08 +0400

    Count of '0' digit in [0, n]

---
+++ Hasmik Kirakosyan/Homework/Digits_number_in_[m,n] (Room number).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+int main () {
+    
+    int n;
+    std::cout<<"N= ";
+    std::cin>>n;
+    n/=10;
+    int sum=0, i=2, k=0, last=n%10;
+    n/=10;
+    
+    while (n>0) {
+          sum=sum+(int) n%10*i*int (pow(10,k));
+          i++;
+          k++;
+          n/=10;
+    }
+    sum=sum*10+last;
+    std::cout<<"\n Count of digit '0' in number is :"<<sum;
+        
+    char ch;
+    std::cin>>ch;
+    
+    return 0;
+}

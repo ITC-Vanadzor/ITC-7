a8d3d91d1672763be0c45ad9c41a6554f690c03c
Hasmikus <hasmikus@gmail.com>
2015-10-26 20:25:00 +0400

    Task 1: Count of 0s at the end of N!

---
+++ Hasmik Kirakosyan/Homework/26.10/Count_0s_at_ the_of_the_number.cpp
@@ -0,0 +1,23 @@
+//How much 0 is at the end of N! 
+
+#include <iostream>
+
+    
+int main () {
+    int N, numOf2=0, numOf5=0, numOf10=0;
+    std::cout<<"Input number :";
+    std::cin>>N; 
+    
+    for (int i=1; i<=N; i++) {
+       if (i%10==0) numOf10++;
+            else    
+                    if (i%10==5) numOf5++;
+   }        
+    std::cout<<"\nNumber of 10 by 2*5 :"<<numOf5<<" \nNumber of 10 in [1,N] is : "<<numOf10; 
+    
+    std::cout<<"\n\nNumber of 0 in N! is : "<<numOf5+numOf10;
+    
+    char ch;
+    std::cin>>ch;
+    return 0;
+}

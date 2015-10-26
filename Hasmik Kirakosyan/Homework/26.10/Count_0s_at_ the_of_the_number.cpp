6d19ecf04195b5d625168d0d0ee91da6e955c546
Hasmikus <hasmikus@gmail.com>
2015-10-26 20:35:59 +0400

    Number of 0s at the end of  N!: Optimization of steps

--- Hasmik Kirakosyan/Homework/26.10/Count_0s_at_ the_of_the_number.cpp
+++ Hasmik Kirakosyan/Homework/26.10/Count_0s_at_ the_of_the_number.cpp
@@ -7,12 +7,13 @@ int main () {
     int N, numOf2=0, numOf5=0, numOf10=0;
     std::cout<<"Input number :";
     std::cin>>N; 
-    
-    for (int i=1; i<=N; i++) {
-       if (i%10==0) numOf10++;
-            else    
-                    if (i%10==5) numOf5++;
-   }        
+    int i=0;
+    do  {
+       if (i%10==0 && i!=0) numOf10++;
+            else 
+                 if (i%10==5)   numOf5++;
+        i+=5;
+   }  while (i<=N);       
     std::cout<<"\nNumber of 10 by 2*5 :"<<numOf5<<" \nNumber of 10 in [1,N] is : "<<numOf10; 
     
     std::cout<<"\n\nNumber of 0 in N! is : "<<numOf5+numOf10;

c6561357cf9f4f3ab93624b01395ea0d2c389d3d
Ani121293 <ani_saghatelyan@mail.ru>
2015-10-27 22:40:12 +0400

    hasvarkman metod@ opxeci yst 5-i astichani

--- Hasmik Kirakosyan/Homework/26.10/Count_0s_at_ the_of_the_number.cpp
+++ Hasmik Kirakosyan/Homework/26.10/Count_0s_at_ the_of_the_number.cpp
@@ -1,22 +1,20 @@
 //How much 0 is at the end of N! 
 
 #include <iostream>
+#include <math.h>
 
     
 int main () {
-    int N, numOf2=0, numOf5=0, numOf10=0;
+    int N, numOf0=0, numOf5=0, numOf10=0;
     std::cout<<"Input number :";
     std::cin>>N; 
     int i=0;
-    do  {
-       if (i%10==0 && i!=0) numOf10++;
-            else 
-                 if (i%10==5)   numOf5++;
-        i+=5;
-   }  while (i<=N);       
-    std::cout<<"\nNumber of 10 by 2*5 :"<<numOf5<<" \nNumber of 10 in [1,N] is : "<<numOf10; 
+    for (i=1;pow(5, i)<=N; i+=2) {
+        numOf0+= N/(int) pow(5, i); 
+    }     
+  //  std::cout<<" \nNumber of 10 in [1,N] is : "<<numOf0; 
     
-    std::cout<<"\n\nNumber of 0 in N! is : "<<numOf5+numOf10;
+    std::cout<<"\n\nNumber of 0 in N! is : "<<numOf0;
     
     char ch;
     std::cin>>ch;

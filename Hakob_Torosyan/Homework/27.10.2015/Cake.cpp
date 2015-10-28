894c03c7c0dc4c260edb27f26b9f25fe15df5752
Hakobot <htorosyan.1990@gmail.com>
2015-10-28 21:30:04 +0300

    Update Cake.cpp

--- Hakob_Torosyan/Homework/27.10.2015/Cake.cpp
+++ Hakob_Torosyan/Homework/27.10.2015/Cake.cpp
@@ -4,18 +4,18 @@ void Cut (int );
 
 int main()
 {
-int number;
-std::cout <<"Please import number of Vazgens friends " << std::endl;
-std::cin >> number;
-Cut(number);
-return 0;
+  int number;
+  std::cout <<"Please import number of Vazgens friends " << std::endl;
+  std::cin >> number;
+  Cut(number);
+  return 0;
 }
 
 void Cut( int n)
 {
-if(n%2 == 0)
-std::cout << "For pieces we will have " << n/2 << std::endl;
-if(n%2 != 0)
-std::cout << "For pieces we will have " << n/2 << std::endl;
+  if(n%2 == 0)
+  std::cout << "For pieces we will have " << n/2 << std::endl;
+  if(n%2 != 0)
+  std::cout << "For pieces we will have " << n/2 << std::endl;
 }
 

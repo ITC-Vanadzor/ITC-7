b03dc12ca5e132d1e8009a13ef6155566358f117
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2015-11-03 12:39:23 +0300

    covapnya tneri poxvac tarberak@

--- Edgar_Hakobyan/Homework/31.10.2015/villa.cpp
+++ Edgar_Hakobyan/Homework/31.10.2015/villa.cpp
@@ -2,24 +2,13 @@
 #include <math.h>
 int tneri_qanak(int x,int y)
 {
-	if(x>y)
-	{
-		return (x-y-2)/2;
-	}
-	else
-	{
-		return (y-x-2)/2;
-	}
+	return (fabs(x-y)-2)/2;
 }
 int main()
 {
 	int n,m;
-	do
-	{
-		std::cout<<"n="; std::cin>>n;
-		std::cout<<"m="; std::cin>>m;
-	}
-	while(fabs(n-m)<4);
+	std::cout<<"n="; std::cin>>n;
+	std::cout<<"m="; std::cin>>m;
 	std::cout<<"Armeni ev Ashoti tneri mijev ka "<<tneri_qanak(m,n)<<" tun"<<std::endl;
 	return 0;
 }

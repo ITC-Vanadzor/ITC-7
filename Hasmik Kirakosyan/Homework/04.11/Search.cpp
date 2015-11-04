ab243389c0a907b23172f65896aabd60e0d75c7b
Hasmikus <hasmikus@gmail.com>
2015-11-05 00:45:11 +0400

    Changed style and step for finding next element

--- Hasmik Kirakosyan/Homework/04.11/Search.cpp
+++ Hasmik Kirakosyan/Homework/04.11/Search.cpp
@@ -4,9 +4,9 @@
 // element - searching element 
 // compare - if function is true, return array element value
 
-bool isEqual (void* a) {  // Function Equal 5.5
-             int*  temp= reinterpret_cast<int*>(a);
-             return (*temp==5);
+bool isEqual (void* a) {  // Function Equal 6.0
+             double*  temp= reinterpret_cast<double*>(a);
+             return (*temp==6.0);
      }
 bool isMoreThan (void* a)  {  // Function for finding is the number >7
              int*  temp= reinterpret_cast<int*>(a);
@@ -16,11 +16,12 @@ bool isMoreThan (void* a)  {  // Function for finding is the number >7
 
 int search (void* begin, void* end, int elementSize,  bool(*compare)(void* element)) {
     
-    int* i = reinterpret_cast<int*>(begin);
-    int* ending = reinterpret_cast<int*>(end);
+    double* i = reinterpret_cast<double*>(begin);
+    double* ending = reinterpret_cast<double*>(end);
  
-    int arr_size=ending-i; //  Find the size of array by end address and begin addres
-    for (int j=1; j<=arr_size; i+=elementSize, j++) {
+    int arr_size=(int(end)-int(begin))/elementSize;
+    std::cout<<"\n Array size : "<<arr_size;  //  Find the size of array by end address and begin addres
+    for (int j=1; j<=arr_size; i+=1, j++) {
         if (compare(i)) {
                               return j;                              
         }
@@ -29,8 +30,8 @@ int search (void* begin, void* end, int elementSize,  bool(*compare)(void* eleme
 
 int main () {
     int a[4]={1,8,20,6};
-    int b[7];
-    std::cout<<"Please, input 7 integer elements of the second array ";
+    double b[7];
+    std::cout<<"Please, input 7 double elements of the second array ";
     for (int i=0; i<7; ++i) {
         std::cin>>b[i];
     }
@@ -42,8 +43,13 @@ int main () {
     bool(*functionMoreThan7)(void*);
     functionMoreThan7 = isMoreThan;
     
-    std::cout<<"First element index that is more than 7 is : "<<search (&a, &a[3], sizeof(int), functionMoreThan7);
-    std::cout<<"\n\n First element index that is equal 5.5 is "<<search (&b, &b[6], sizeof(double), functionEqual);
+    std::cout<<"\n\n First element index that is equal 6.0 is "<<search (&b, &b[7], sizeof(double), functionEqual)<<std::endl;
+    
+    std::cout<<"\nArray of ints ";
+    for (int i=0; i<4; ++i) {
+        std::cout<<" "<<a[i];
+    }
+    std::cout<<"\nFirst element index that is more than 7 is : "<<search (&a, &a[4], sizeof(int), functionMoreThan7);
     
     
  char ch;

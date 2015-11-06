2e19a92516e955d42872c51864a430c21373a388
Vagharsh75 <vagharsh75@gmail.com>
2015-11-06 09:59:12 +0300

    Update revert_n.cpp

--- Vagharsh_Hakobyan/Homework/20151105/revert_n.cpp
+++ Vagharsh_Hakobyan/Homework/20151105/revert_n.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+void _revers(int &a,int &sum)
+{
+  if (a>0)
+    {
+      sum = sum*10+a%10;
+      a=a/10;
+      _revers(a,sum);
+    }
+}
+
+int revers(int a)
+{
+  int sum = 0;
+  _revers(a,sum);
+  return sum;
+}
+
+int main()
+{  
+  int n = 0;
+  std::cout << "Please import n " << std::endl;
+  std::cin >> n;
+  std::cout << revers(n) << std::endl;
+  return 0;
+}

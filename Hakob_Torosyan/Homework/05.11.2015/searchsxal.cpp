7a683c89daf3bf1edbefbf5d730e45f35e862723
Hakobot <htorosyan.1990@gmail.com>
2015-11-06 13:29:27 +0300

    Update searchsxal.cpp

--- Hakob_Torosyan/Homework/05.11.2015/searchsxal.cpp
+++ Hakob_Torosyan/Homework/05.11.2015/searchsxal.cpp
@@ -19,7 +19,7 @@ int int_compare (void* element_1, void* element_2) {
 	return 2;
 	if(*((int*)element_1) >  *((int*)element_2))
 	return 3;
-}
+	}
 
 
 
@@ -35,10 +35,10 @@ int search(void* begin, void* end, size_t elementsize, void* element, int (*comp
 				return i;			
 		}
 		if (compare(mid,element) == 2){
-		return search(mid+1, end,  elementsize, element, compare(mid+elementsize, element));
+		return search(mid, end,  elementsize, element, compare);
 		}	
 		if (compare(mid,element) == 3){
-		return search(begin, mid-1, elementsize,  element,  compare(mid-elementsize, element ));
+		return search(begin, mid, elementsize,  element,  compare);
 		}
 	}	
 	return -1;
@@ -50,13 +50,14 @@ int main () {
 	double doubleNumber = 7.6;
  	int index_result2 = search (doubleArr, doubleArr + 4, sizeof(double), &doubleNumber, double_compare);
 	std::cout << "\n Element index equal  7.6 is " << index_result2 << std::endl;	
-	
+	//work wrong for int
 	int intArr [] = {1, 5, 7, 9, 10, 77, 89};
-	int intNumber = 2;
+	int intNumber = 10;
 	int index_result3 = search (intArr, intArr + 6, sizeof(int), &intNumber, int_compare);
 	std::cout << "\n Element intdex equal 2 is " << index_result3 << std::endl;
-
+	
 
 
 return 0;
 }
+

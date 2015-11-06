41198e7e84ecd3cefdb55068a7119e99080c963e
jiro <jiro-19944@mail.ru>
2015-11-07 01:34:36 +0400

    avelacreci  bajanarar@ ev bazmapatik@ 1 cragrum

---
+++ Jirayr_Stepanyan/Homework/06_11_2015/amenamec_@ndhanur_bajanarar.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+int bajanarar(int min, int a, int b)
+{
+	if((a % min) == 0 && (b % min) == 0)
+	{
+		return min;
+	}
+	bajanarar(min - 1, a, b);
+}
+int bazmapatik(int max, int a, int b)
+{
+	if((max % a) == 0 && (max % b) == 0)
+	{
+		return max;
+	}
+	bazmapatik(max + 1, a, b);
+}
+int main()
+{
+	int n = 0, m = 0;
+	while(n <= 0 || m <= 0)
+	{
+		std::cout << "tpel voreve bnakan amboxj tiv n = ";
+		std::cin >> n;
+		std::cout << "tpel voreve bnakan amboxj tiv n = ";
+		std::cin >> m;
+	}
+	int min = m;
+	int max = n;
+	if(n <= m)
+	{
+		min = n;
+		max = m;
+	}
+	int mec_bajanarar = bajanarar(min, n, m);
+	std::cout << "amenamec @ndhanur bajanarar@ = " << mec_bajanarar << std::endl;
+	int poqr_bazmapatik = bazmapatik(max, n, m);
+	std::cout << "amenapoqr @ndhanur bazmapatik@ = " << poqr_bazmapatik << std::endl;
+	
+   return 0;
+}
+

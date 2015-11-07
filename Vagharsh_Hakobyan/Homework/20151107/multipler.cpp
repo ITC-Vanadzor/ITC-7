e5c96622b400682d9b47345a28fef4e82131cb79
Vagharsh75 <vagharsh75@gmail.com>
2015-11-07 09:28:17 +0400

    add divider and multipler

---
+++ Vagharsh_Hakobyan/Homework/20151107/multipler.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int divider(int a, int b)
+{
+  if(a>b)
+    {
+      a=a-b;
+      return divider(a,b);
+    }
+  if(a<b)
+    {
+      b=b-a;
+      return divider(a,b);
+    }
+  if(a==b)
+    {
+      return a;
+    }
+}
+
+
+int main()
+{
+  int a = 2, b =3;
+  std::cout << "Please import a " << std::endl;
+  std::cin >> a;
+  std::cout << "Please import b " << std::endl;
+  std::cin >> b;
+  std::cout << "For divider we will have "; 
+  std::cout << a*b/divider(a,b) << std::endl;
+  return 0;
+}

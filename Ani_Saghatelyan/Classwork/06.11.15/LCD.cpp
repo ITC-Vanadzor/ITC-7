7cc67a97784ff34e4810b1673696c8f9f96f7eb2
Ani121293 <ani_saghatelyan@mail.ru>
2015-11-07 04:25:14 +0400

    Changed the functions, which determines LCM for two numbers,Here  isn't using GCD function

--- Ani_Saghatelyan/Classwork/06.11.15/LCD.cpp
+++ Ani_Saghatelyan/Classwork/06.11.15/LCD.cpp
@@ -1,16 +1,23 @@
-// amenapoqr yndhanur  bazmapatik
+// The programm determines the LCM for 2 numbers using recursia
 
 # include <iostream>
 
-int GCD(int a, int b){
-   if(b==0){
+int CurrentLCM(int a, int b, int current){// current function in order to get just two arguments in Function  LCD
+   if( a%current==0  && a%b==0){
         return a;
       }
+   
   else {
-      return GCD(b, a%b);
+      return CurrentLCM(current%b+a,b,current);
       }
     }
-    
+
+int LCM(int A, int B){
+   int currentNumber=A;
+   CurrentLCM(A,B,currentNumber);  
+    }
+
+
     int main()
  {
   int x;
@@ -21,7 +28,7 @@ int GCD(int a, int b){
       std::cout<<"Enter y\t";
       std::cin>>y;
        }while(x==0|| y==0);
-       int answer=  (x*y)/GCD(x,y) ;    
+       int answer= LCM(x,y);    
   std::cout<<answer<<std::endl;
   return 0;
    }

7af17919c26442dcb31a8da292699b91006a96cc
Vagharsh75 <vagharsh75@gmail.com>
2015-11-23 15:55:07 +0300

    Create realloc.cpp

---
+++ Hakob_Torosyan/C++ solutions/Classwork/23.11.2015/realloc.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+int main()
+{	
+  std::cout << "Input count of int numbers n= ";
+  int n;
+  std::cin >> n;
+  int* ptr1 = (int*)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; ++i)
+    {
+      std::cin >> ptr1[i];
+    }
+
+  for (int i = 0; i < n; ++i)
+    {
+      std::cout << ptr1[i] <<" ";
+    }
+  std::cout << std::endl;
+
+
+  std::cout << "Input new count of int numbers m= ";
+  int m;
+  std::cin >> m;
+  int* ptr2 = (int*)malloc(sizeof(int) * m);
+  int i =0;
+  while(i < n && i < m)
+    {
+	  ptr2[i] = ptr1[i];
+	  ++i;
+    }
+  free(ptr1);
+  int l;
+  if (m <= n)
+    {
+      l = m;
+    }
+  else
+    {
+      l = n;
+    }
+  for (int i = 0; i < l; ++i)
+    {
+      std::cout << ptr2[i] << " ";
+    }
+  std::cout << std::endl;
+  free(ptr2);
+  
+  return 0;
+}

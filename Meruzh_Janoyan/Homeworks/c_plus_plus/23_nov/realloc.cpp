6dce7e1621357358c44d9e0fd67ed9fbb7a71be4
Janoyan <meruzh2008@rambler.ru>
2015-11-24 13:25:07 +0400

    function realloc()

---
+++ Meruzh_Janoyan/Homeworks/c_plus_plus/23_nov/realloc.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <stdlib.h>
+
+void *my_realloc(void *prev,int size){
+	
+	if(size>0){
+		void *cp=malloc(size);
+		void *ptr=cp;
+		void *cpprev=prev;
+
+	for(int i=0;i<size;++i){
+			ptr+=1;
+			prev+=1;
+			*((char*) ptr)=*((char*) prev);
+			}
+	free(cpprev);
+
+	return cp;
+	}
+	else{
+		free(prev);
+		return NULL;
+	}
+}
+int main()
+{
+ int *p=(int*)malloc(sizeof(int)*10);
+	
+	for(int i=0;i<10;++i) {
+			*(p+i)=i;
+			std::cout<<*(p+i);
+	}
+	std::cout<<std::endl;
+	p=(int*)my_realloc(p,sizeof(int)*5);
+	//p=(int*)realloc(p,sizeof(int)*30);
+ 	if(p!=NULL) {
+	for(int i=0;i<5;++i) {
+			//*(p+i)=i+65;
+			std::cout<<*(p+i);
+	}
+	std::cout<<std::endl;
+ }
+	free(p);
+
+return 0;
+}

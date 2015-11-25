a984066764fc62c6ca0afb182617a5d33a767baf
Ani Saghatelyan <Ani121293>
2015-11-25 15:36:49 +0400

    Added the constructer with 1 parameter, change some lines in main for testing

--- Ani_Saghatelyan/Homework/c++/24.11.15/Shared_Ptr.cpp
+++ Ani_Saghatelyan/Homework/c++/24.11.15/Shared_Ptr.cpp
@@ -22,47 +22,57 @@ struct Sptr{
    	//*************default constructer************
     Sptr():ptr(new Type),counter(new int(1)){ 						 
      	std::cout<<"Called "<<this<<" Sptr constructer "<<*(counter)<<" \n";
-        *(this->counter)=1;
+       // *(this->counter)=1;
     }
+    
+    	//************* constructer with one parameter************
+    Sptr(Type *rhs):ptr(rhs),counter(new int(1)){ 						 
+     	std::cout<<"Called "<<this<<" Sptr constructer  with one parameter"<<*(counter)<<" \n";
+       // *(this->counter)=1;
+    }
+    
   	 //***************** operator =*****************
     Sptr& operator=(Sptr& rhs){ 	
-         if(this->counter!=rhs.counter){// it has already been = to other object, which has been  pointed to that pointer of heap-memory  										
-		   this->ptr=rhs.ptr;
-		   this->counter=rhs.counter;
-		   *(this->counter)+=1;
+         if(counter!=rhs.counter){// it has already been = to other object, which has been  pointed to that pointer of heap-memory  										
+		   ptr=rhs.ptr;
+		   counter=rhs.counter;
+		   *(counter)+=1;
            } 
 		    std::cout<<"Operator= "<<this<<"  "<<*(counter)<<" \n";	
            return *this;
     }
       	
  	 //********************copy constructer**************	
-    Sptr( Sptr& rhs):ptr(rhs.ptr){ 
-        this->counter=rhs.counter;
-        *(this->counter)+=1;							
+    Sptr(const Sptr& rhs){ 
+    ptr=rhs.ptr;
+       counter=rhs.counter;
+        *(counter)+=1;							
         std::cout<<"Called "<<this<<" copy constructer  "<<*(counter)<<" \n";
     } 
   	 //***********************Destructer*********
     ~Sptr(){
      	 std::cout<<"Called "<<this<<" destructer "<<*(counter)<<" \n";
-           if(*(this->counter)==1){
-		  delete(this->ptr);
-		  delete(this->counter);
+           if(*(counter)==1){
+		  delete(ptr);
+		  delete(counter);
             }
            else {
-                      *(this->counter)-=1;
+                      *(counter)-=1;
             }
     }
  };
     
            
 int main(){
-        Sptr S;
+        Sptr*k=new Sptr;
+        Type* t =new Type;
+        Sptr S(t);
 	Sptr X(S);
 	Sptr Y;
         Y=S;
         Sptr P(X);
         P=S;
-        
+             
  
 return 0;
 }

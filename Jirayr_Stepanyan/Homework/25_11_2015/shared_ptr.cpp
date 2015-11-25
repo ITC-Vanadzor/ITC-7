3bbe36b316f380dcd8cfe57fc3a3c7e0ec76426d
jiro <jiro-19944@mail.ru>
2015-11-25 23:21:27 +0400

    avelacreci shared_ptr

--- Jirayr_Stepanyan/Homework/25_11_2015/shared_ptr.cpp
+++ Jirayr_Stepanyan/Homework/25_11_2015/shared_ptr.cpp
@@ -1,36 +1,68 @@
 #include <iostream>
-#include <cstdlib>
-static int count = 0;
-struct sptr
+struct Sptr
 {
+	int* count;
 	int* x;
-	sptr(int* y);
-	~sptr();
+	Sptr();
+	Sptr(Sptr* y);
+	Sptr(Sptr& k);
+	Sptr& operator = (const Sptr& h);
+	~Sptr();
 };
-sptr::sptr(int* y)
-	:x(y)
+Sptr::Sptr()
+	:count(new int(1))
+	,x(new int)
 {
-	std::cout << "stexceci" << std::endl;
-	++count;
+	
+	std::cout << "datark konstruktor 	";
+	std::cout << "count = " << *count << std::endl;
 }
-sptr::~sptr()
+Sptr::Sptr(Sptr* y)
+	:x(y -> x)
+	,count(new int(1))
 {
-	std::cout << "jnjeci" << std::endl;
-	if(count == 0)
+	std::cout << "kostruktor	";
+	std::cout << "count = " << *count << std::endl;
+}
+Sptr::Sptr(Sptr& k)
+{
+	x = k.x;
+	count = k.count;
+	++(*count);
+	std::cout << "copy 	";
+	std::cout << "count = " << *count << std::endl;
+}
+Sptr& Sptr:: operator = (const Sptr& h)
+{
+	x = h.x;
+	count = h.count;
+	++(*count);
+	std::cout << "operator = 	";
+	std::cout << "count = " << *count << std::endl;
+}
+Sptr::~Sptr()
+{
+	if(--(*count) == 0)
 	{
+		std::cout << "jnjeci  DESTRUKTOR	";
+		std::cout << "count = " << *count << std::endl;
 		delete x;
+		delete count;
 	}
 	else
 	{
-		--count;
+		std::cout << "pakasacreci count  DESTRUKTOR	" ;
+		std::cout << "count = " << *count << std::endl;
 	}
+	
 }
 int main()
 {
-	sptr* a = new int;
-	sptr b(a);
-	std::cout << count << std::endl;
-	delete a;
+	Sptr a(new Sptr);
+	Sptr b(a);
+	Sptr c(b);
+	Sptr n;
+	n = c;
 
    return 0;
 }

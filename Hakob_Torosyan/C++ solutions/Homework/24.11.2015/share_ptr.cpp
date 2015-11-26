fa56a30fe3bd8ec9291eebdead238f05f2d6dcd6
Hakobot <htorosyan.1990@gmail.com>
2015-11-26 14:37:06 +0300

    Update share_ptr.cpp

--- Hakob_Torosyan/C++ solutions/Homework/24.11.2015/share_ptr.cpp
+++ Hakob_Torosyan/C++ solutions/Homework/24.11.2015/share_ptr.cpp
@@ -1,31 +1,60 @@
 #include <iostream>
 #include <cstdlib>
 
-struct my_type {
-	int a;
+
+struct share_ptr
+{
+	int* Ptr;
+        int* counter;
+ 	share_ptr()
+	  :Ptr(NULL), counter(new int(1))
+	{  
+	  std::cout << "Gives 1 value to counter " << *counter << std::endl;
+	  *counter = 1;
+	};
+share_ptr& operator = (share_ptr & k)
+  {        if(Ptr!=k.Ptr)
+                {
+           		std::cout << "Created new pointer "<< Ptr << std::endl;
+			counter = k.counter;
+			++(*counter);
+		}
+		 else
+		 {
+			std::cout << "Wrong seting " << std::endl;
+		 }
+    }
+  	share_ptr(share_ptr& a)
+  	{
+	       	std::cout << "Created new pointer "<< Ptr << std::endl;
+		counter = a.counter;
+		++(*counter);
+  	};
+  	~share_ptr()
+  	{
+	   --(*counter);
+	  if(*counter==0)
+	        {  
+  			delete Ptr;
+			delete counter;
+    			std::cout << "Deleted pointer " << std::endl;
+        	}
+	  std::cout << "Deleted last pointer " << std::endl;
+  	};
 };
-struct sPtr {
-	my_type *pointer;
-	int      counter;
-    	sPtr();
-	~sPtr();
-};
 
-sPtr::sPtr()
-	{
-		std::cout << " Create new object " << std::endl;
-		counter++;
-	}
-sPtr::~sPtr()
-	{
-		std::cout << "Delete object " << std::endl;
-		counter--;
-	}
-
-
-int main() {
-    
-
-
-return 0;
+int main()
+{
+  	share_ptr a;
+  	a.Ptr = new int;
+  	std::cout << a.Ptr <<" before " << std::endl;
+  	share_ptr d(a);
+  	std::cout << a.Ptr << " after " << std::endl;
+  	share_ptr e(d);
+  	share_ptr f(e);
+ 	share_ptr c;
+  	c = f;
+	c = c;
+  	return 0;
 }
+

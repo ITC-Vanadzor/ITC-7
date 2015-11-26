3394102f3843b3d40a5c18cd70c2e9600c00eac5
AnushQalashyan <anushqalashyan@gmail.com>
2015-11-26 14:17:43 +0300

    Update Realloc.cpp

--- Araksya_Hambaryan/Homework/C++/Homework_23_11_2015/Realloc.cpp
+++ Araksya_Hambaryan/Homework/C++/Homework_23_11_2015/Realloc.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 #include <cstdlib>
-int min(int a, int b) {
-    return a < b ? a : b;
-}
+
+
 void* realloc2(void* OldArr,int OldSize, int NewSize)
 {
     void* NewArr;
-    if (NewSize ==0) {
+    if (NewSize == 0) {
 	return NULL;
     }
-    if(OldArr==NULL){
+    if (OldArr == NULL){
 	return malloc(NewSize);
-    } else{
+    }
 	NewArr = malloc (NewSize);
-	for(int i = 0; i < min(OldSize, NewSize)/sizeof(int); ++i)
+	for (int i = 0; i < (OldSize < NewSize ? OldSize : NewSize)/sizeof(int); ++i)
 	{
-	    *((int*)NewArr+i) = *((int*)OldArr+i);
+	    *((int*)NewArr + i) = *((int*)OldArr + i);
 	}
 
 	free(OldArr);
-    }
+    
     return NewArr;
 }
 int main()  {
     int* x = (int*) malloc (4 * sizeof(int));
-    for (int i=0; i<4; ++i)
+    for (int i = 0; i < 4; ++i)
     {
 	x[i] = i + 1;
     }
-    for(int i = 0; i < 4; ++i)
+    for (int i = 0; i < 4; ++i)
     {
 	std::cout << "x["<< i << "]= "<< x[i] << std::endl;
     }
-    int* y = (int*)realloc2(x, 4*sizeof(int), 5 * sizeof(int));
-    for(int i = 0; i < 5; ++i)
+    int* y = (int*)realloc2(x, 4*sizeof(int), 10 * sizeof(int));
+    for (int i = 0; i < 10; ++i)
     {
 	std::cout << "y[" << i << "]= " <<  y[i] << std::endl;
     }

030270c806bed31e7a32045eccf39326ceaebb12
Hasmik <hasmikus@gmail.com>
2015-11-26 22:52:37 +0400

    Operator+.cpp

--- Hasmik_Kirakosyan/Homework/C++/24.11/Operator+.cpp
+++ Hasmik_Kirakosyan/Homework/C++/24.11/Operator+.cpp
@@ -25,6 +25,9 @@ A.b=2;
 objType B;
 B.a=7;
 B.b= 8;
+
+std::cout<<" A.a ="<<A.a<<" A.b ="<<A.b<<std::endl;
+std::cout<<" B.a ="<<B.a<<" B.b ="<<B.b<<std::endl;
 operator_plus C;
 C=A+B;
 std::cout<<"C = A+B | C.a = "<<C.a<<" D.b = "<<C.b<<std::endl;

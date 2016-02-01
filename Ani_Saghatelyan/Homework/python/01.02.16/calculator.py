c0f962b1919c1f12cd73a94dcf01e50a88d7c99d
Ani121293 <ani_saghatelyan@mail.ru>
2016-02-02 03:58:26 +0400

    Added programms writen in python. the programms are calculator, string, file sorting

---
+++ Ani_Saghatelyan/Homework/python/01.02.16/calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+
+#define functions 
+def Sum(a,b):
+	 print   a+b
+
+def Sub(a,b):
+	print   a-b
+
+def Mult(a,b):
+	print  a*b
+
+def Div(a,b):
+        if b == 0:
+		print "can't divide to 0"
+                exit
+        else:
+    		print   a/b
+
+# entering  the numbers for doing arithmetic operation
+x=input("Enter the  first number \t")
+y=input("Enter the second number \t")
+# making case  for doing operation
+options={0 : Sum, 1 : Sub, 2 : Mult, 3 : Div, }		
+# entering the choice for operation
+choice=input("Enter the choice of arithmetic operation.\n0 for sum \n1 for sub, \n2 for mult,\n3 for  div \t")
+# calling function
+options[choice](x,y)
+
+
+
+

71e9fb34855f276f247195a19f73f3f344104175
hasmikus@gmail.com <Hasmik>
2016-02-01 23:54:44 +0400

    Added sortNames program. Details: 	-Find *.txt files in current folder 	-Append names and surnames written in .txt files to one list 	-Sort list items

---
+++ Hasmik_Kirakosyan/Homework/Python/sortNames.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+import glob,os
+
+list=[]
+os.chdir("/home/hasmik/tests")
+for file in glob.glob("*.txt"):
+	with open(file, "r") as f:
+		for line in f:
+			list.append(line[:-1])
+print "\nNot sorted version"
+print "===================="
+for x in list:
+	print x
+print "\nSorted version"
+print "===================="
+list.sort()
+for x in sorted(list):
+	print x
+

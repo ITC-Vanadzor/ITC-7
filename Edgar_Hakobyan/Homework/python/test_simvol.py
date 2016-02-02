a6f03bc456802198eba2c28e9bee9937349472b4
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-02-02 23:58:06 +0400

    new version test_simvol

--- Edgar_Hakobyan/Homework/python/test_simvol.py
+++ Edgar_Hakobyan/Homework/python/test_simvol.py
@@ -1,13 +1,14 @@
 def test(text):
-	simvol = ['!','@','$','%','^','(',')']
-	for i in range(len(simvol)):
-		if text == simvol[i]:
-			return 1
-	return 2
-text = "asd%h$d^hj)"
+    simvol = ['!','@','#','$','%','^','*','(',')','-','+','|']
+    for i in simvol:
+        if text == i:
+            return 1
+    return 2
+text = raw_input("write text! ")
 newtext = ''
 print "old text ",text
-for i in range(len(text)):
-	if test(text[i]) == 2:
-		newtext = newtext + text[i];
+for i in text:
+    if test(i) == 2:
+        newtext = newtext + i;
 print "new text ",newtext
+

8ff6274daa7bb54ec5bd2b7c3b6316790b34d038
jiro <jiro-19944@mail.ru>
2016-02-03 22:16:23 +0400

    edited re.match.py

--- Jirayr_Stepanyan/Homework/python/03_02_2016/re_match.py
+++ Jirayr_Stepanyan/Homework/python/03_02_2016/re_match.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 import re
-from os import listdir
-f=open("brend2.txt", "r+");
-a=f.read();
-i=0;
-if not re.match("Brand: [A-Z][A-Z]*|\n|Version: [0-9].[0-9]*|\n|Made by: [A-Z][a-z]*", a) is None:
-    print "ok";
+import sys
+
+try:
+    f = open(sys.argv[1], "r+");
+    a = f.read();
+except IOError:
+    print("ERROR!!! there are not ", sys.argv[1], " file");
 else:
-    print "no";
+    if not re.match("Brand: [A-Z][A-Z]*|\n|Version: [0-9].[0-9]*|\n|Made by: [A-Z][a-z]*", a) is None:
+        print("ok");
+    else:
+        print("no");
+
+	f.close();

b4094a6b3c6f156b2c28677e30ed1487b1963424
Smbat888 <sargsyanyan@mail.ru>
2016-02-03 21:28:03 +0300

    Update shablon.py
    change my error in IOError

--- Smbat_Sargsyan/homework/Python/shablon.py
+++ Smbat_Sargsyan/homework/Python/shablon.py
@@ -4,10 +4,11 @@ try:
 	file1=open(sys.argv[1],"r+")
 	
 	f=file1.read()
-except: 
-	print("Not a file")
+except IOError:
+	print("Cannot open file")
 shablon_re=re.compile(r"Brend: [A-Z]*|\n|Version: [1-9].[0-9].[0-9]*|\n|Made: [A-Z][a-z]*")
 if shablon_re.match(f) is None:
     print("ERROR!!!")
 else:
 	print("It's OK")
+file1.close()

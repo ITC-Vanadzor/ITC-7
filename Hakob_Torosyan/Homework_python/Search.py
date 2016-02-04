83a4803aa0f541f8059dbb450571b36ef47e1d37
Hakobot <htorosyan.1990@gmail.com>
2016-02-04 13:15:09 +0300

    Update Search.py

--- Hakob_Torosyan/Homework_python/Search.py
+++ Hakob_Torosyan/Homework_python/Search.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 import re
 from os import listdir
-f=open("sevagir.txt", "r+")
+try:
+	f=open("sevagir.txt", "r+")
+except IOError:
+	print " ERROR "
 i=0;
 line1=0
 line2=0
@@ -14,7 +17,7 @@ for line in f:
         line2=line
     if i==2:
         line3=line
-    if i==3
+    if i==3:
 	line4=line
     i+=1
 pattern1 = '^Program Name: | | [A-Z] [a-z]*+ $' 
@@ -22,6 +25,8 @@ pattern2 = '^Version: | | [\d\.]+ $'
 pattern3 = '^Programmer: | | [A-Z] [a-z]* | | [A-Z] [a-z]*yan $'
 pattern4 = '^Company: | | [A-Z]* $' 
 if   not re.match(pattern1,line1) is None and not re.match(pattern2,line2) is None and not re.match(pattern3,line3) is None and not re.match(pattern4,line4) is None:
-print "We have right template: "
+	print "We have right template: "
+
  
+
 

a84cac8ae2f9c9f0427b7d6f49aeb60f86db3461
Araksya-Hambaryan <araksyahambaryan@mail.ru>
2016-02-04 13:14:19 +0300

    Update Files.py

--- Araksya_Hambaryan/Homework/Python/01_02_2016/Files.py
+++ Araksya_Hambaryan/Homework/Python/01_02_2016/Files.py
@@ -2,15 +2,21 @@
 import glob, re, os
 
 data=[]
-for file_txt in glob.glob('./*.txt'):
-    tmp = open(file_txt, 'r')
-    for line_txt in tmp:
-        line_data = line_txt.rstrip()
-        count = line_data.find(' ')
-        if i != -1 and not re.match('^[A-Z][a-z]+\s[A-Z][a-z]+$', line_data) is None :
-            data.append(line_data)
+path = raw_input('dir path")
+try:
+    if os.getcwd() != path :
+        os.chcwd(path)
+except FileNotFoundError:
+    print 'path not found'
 
-    tmp.close()
+for file_txt in glob.glob('./*.txt'):
+    with open(file_txt, 'r') as tmp:
+        for line_txt in tmp:
+            line_data = line_txt.rstrip()
+            count = line_data.find(' ')
+            if count != -1 and not re.match('^[A-Z][a-z]+\s[A-Z][a-z]+$', line_data) is None :
+                data.append(line_data)
+
 data.sort()
 data_file = open('./log', 'rw')
 data_file.write(data)

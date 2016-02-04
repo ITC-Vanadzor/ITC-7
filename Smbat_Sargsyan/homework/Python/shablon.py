c6b4877a74c84ce0b9fdfe22b5a19ac0d4d542f3
smbat <jkkjh@mail.ru>
2016-02-05 01:22:28 +0400

    update shablon program

--- Smbat_Sargsyan/homework/Python/shablon.py
+++ Smbat_Sargsyan/homework/Python/shablon.py
@@ -1,16 +1,16 @@
 import re
 import sys
 try:
-	file1=open(sys.argv[1],"r+")
-	
+	file1=open(sys.argv[1],"r")
 	f=file1.read()
 except IOError:
-<<<<<<< HEAD
 	print("Not a file")
-=======
 	print("Cannot open file")
->>>>>>> b4094a6b3c6f156b2c28677e30ed1487b1963424
-shablon_re=re.compile(r"Brend: [A-Z]*|\n|Version: [1-9].[0-9].[0-9]*|\n|Made: [A-Z][a-z]*")
+shablon_re=re.compile(r"""
+Brend: [A-Z]+|\n
+|Version: [1-9][.][0-9][.][0-9]+|\n
+|Made: [A-Z][a-z]+
+""",re.VERBOSE)
 if shablon_re.match(f) is None:
     print("ERROR!!!")
 else:

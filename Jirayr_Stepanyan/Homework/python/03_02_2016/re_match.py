3368c2f4da0b83514c253956a5776605d9c338ce
jiro <jiro-19944@mail.ru>
2016-02-05 02:19:18 +0400

    edited re_match.py

--- Jirayr_Stepanyan/Homework/python/03_02_2016/re_match.py
+++ Jirayr_Stepanyan/Homework/python/03_02_2016/re_match.py
@@ -3,12 +3,12 @@ import re
 import sys
 
 try:
-    f = open(sys.argv[1], "r+");
+    f = open(sys.argv[1], "r");
     a = f.read();
 except IOError:
     print("ERROR!!! there are not ", sys.argv[1], " file");
 else:
-    if not re.match("Brand: [A-Z][A-Z]*|\n|Version: [0-9].[0-9]*|\n|Made by: [A-Z][a-z]*", a) is None:
+    if not re.match("Brand: [A-Z][A-Z]*|\n|Version: [0-9][\.][0-9]*|\n|Made by: [A-Z][a-z]*", a) is None:
         print("ok");
     else:
         print("no");

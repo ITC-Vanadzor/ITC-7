a1d925e5b3ef518013c1c7febae8e2995d5b3277
Smbat888 <sargsyanyan@mail.ru>
2016-02-04 09:38:02 +0300

    Update calculator.py
    updateing calculator

--- Smbat_Sargsyan/homework/Python/calculator.py
+++ Smbat_Sargsyan/homework/Python/calculator.py
@@ -3,13 +3,10 @@ def sum(m,n):
 def hanum(m,n):
 	return m-n
 def bajanum(m,n):
-	try:
-		if n==0:
-			print("ERROR,try again")
-		else :
-			return m/n
-	except ZeroDivisionError:
-			print("ERROR!!!")
+	if n==0:
+		print("ERROR,try again")
+	else :
+		return m/n
 def bazmapatkum(m,n):
 	return m*n
 try:
@@ -18,15 +15,14 @@ try:
 	x=int(input("1)Gumarum" "2)Hanum" "3)Bajanum" "4)Bazmapatkum  ",))
 except ValueError:
 	print("Number,Please")
-try:
-	if x == 1:
-		print sum(k,g)	
-	elif x == 2:
-		print hanum(k,g)
-	elif x == 3:
-		print bajanum(k,g)
-	elif x == 4:
-		print bazmapatkum(k,g)
-	else:
-except ValueError('unknown token %s' % x):
-	print("ERROR!!!)
+if x == 1:
+	print sum(k,g)	
+elif x == 2:
+	print hanum(k,g)
+elif x == 3:
+	print bajanum(k,g)
+elif x == 4:
+	print bazmapatkum(k,g)
+else:
+	except ValueError('unknown token %s' % x):
+		print("ERROR!!!)

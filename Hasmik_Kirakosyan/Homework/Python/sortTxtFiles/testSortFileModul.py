e76077805499d11706288cb498784e2e3c3d2a35
Hasmikus <hasmikus@gmail.com>
2016-02-05 14:20:25 +0400

    Update unitTest

--- Hasmik_Kirakosyan/Homework/Python/sortTxtFiles/testSortFileModul.py
+++ Hasmik_Kirakosyan/Homework/Python/sortTxtFiles/testSortFileModul.py
@@ -2,20 +2,22 @@
 import unittest
 import re
 import sortFileModul
-import sys, os
+import os.path
 
 class TestStringMethods(unittest.TestCase):
 	
 	def test_sortFiles(self):
 		try:
-			os.remove("outputFile")
+			if (os.path.isfile("outputFile")):
+				os.remove("outputFile")
 			sortFileModul.fileCheck('NameSurname1.txt')
 			out = open("outputFile", "r")
 		except IOError:
 			print "File doesnt exist"
 		else: 
 			for line in out:
-				self.assertTrue(re.match('[A-Z][a-z]+ [A-Z][a-z]+', line)) 
+				self.assertRegexpMatches(line,'[A-Z][a-z] [A-Z][a-z]$', msg=None) 
+			out.close()
 
 if __name__=='__main__':
 	unittest.main()	

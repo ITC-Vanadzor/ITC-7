8b498937964cddf52ef5e4f3897f421fde8e0ab6
hasmikus@gmail.com <Hasmik>
2016-02-06 00:00:57 +0400

    Update unittest task

--- Hasmik_Kirakosyan/Homework/Python/sortTxtFiles/testSortFileModul.py
+++ Hasmik_Kirakosyan/Homework/Python/sortTxtFiles/testSortFileModul.py
@@ -2,7 +2,7 @@
 import unittest
 import re
 import sortFileModul
-import os.path
+import os, os.path
 
 class TestSortFilesNames(unittest.TestCase):
 	def setUp(self):
@@ -16,26 +16,48 @@ class TestSortFilesNames(unittest.TestCase):
 			sortFileModul.fileCheck(fileName)
 			try:
 				out = open("outputFile", "r")
-				for line in out:
-					print line
-					self.assertRegexpMatches(line[:-1], "^[A-Z][a-z]+ [A-Z][a-z]+$")
+				resultList = []
+				if (os.stat("outputFile").st_size != 0):
+					for line in out:
+						resultList.append(line[:-1])
 				out.close()
+				return resultList
 			except IOError:
 				print "File doesn't exist"
 		
 
-	#def test_goodCase(self):
-	#		self.__TestCaseTemplete("NameSurname1.txt")
+	def test_goodCase(self):
+			checkList = self.__TestCaseTemplete("NameSurname1.txt")
+			if not checkList:
+				self.assertTrue(True)
+			else:
+				for nameSurname in checkList:
+					self.assertRegexpMatches(nameSurname, "^[A-Z][a-z]+ [A-Z][a-z]+$")
 
-	#def test_onlyName(self):
-	#		self.__TestCaseTemplete("NameSurname2.txt")
-	
-	#def test_emptyFile(self):
-	#		self.__TestCaseTemplete("NameSurname3.txt")
+	def test_onlyName(self):
+			checkList = self.__TestCaseTemplete("NameSurname2.txt")
+			if not checkList:
+				self.assertTrue(True)
+			else:
+				for nameSurname in checklist:
+					self.assertRegexpMatches(nameSurname, "^[A-Z][a-z]+ [A-Z][a-z]+$")
+
+	def test_emptyFile(self):
+			checkList = self.__TestCaseTemplete("NameSurname3.txt")
+			if not checkList:
+				self.assertTrue(True)
+			else:
+				for nameSurname in checklist:
+					self.assertRegexpMatches(nameSurname, "^[A-Z][a-z]+ [A-Z][a-z]+$")
 
 	def test_numbersInFile(self):
-			self.__TestCaseTemplete("NameSurname4.txt")
-	
+			checkList = self.__TestCaseTemplete("NameSurname4.txt")
+			if not checkList:
+				self.assertTrue(True)
+			else:
+				for nameSurname in checklist:
+					self.assertRegexpMatches(nameSurname, "^[A-Z][a-z]+ [A-Z][a-z]+$")
+
 			
 if __name__=='__main__':
 	unittest.main()	

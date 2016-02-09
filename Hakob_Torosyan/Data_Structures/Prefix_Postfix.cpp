e6f762ce134d0da3a97cdf3eaf53c416126ef1ab
Hakob Torosyan <htorosyan.1990@gmail.com>
2016-02-10 02:31:38 +0400

    Add incomplate version 2

--- Hakob_Torosyan/Data_Structures/Prefix_Postfix.cpp
+++ Hakob_Torosyan/Data_Structures/Prefix_Postfix.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
-#include <cstring>
-void Prefix(string &);
-void Postfix(string &);
+#include <string>
+void Prefix(std::string &);
+//void Postfix(string &);
 int main()
 {
-	string str;
+	std::string str;
 	std::cout << "Please import expression: ";
 	std::cin >> str;
 	Prefix(str);
-	Postfix(str);
+	//Postfix(str);
 	return 0;
 }
 
-void Prefix(string &str)
+void Prefix(std::string &str)
 {
 	char stack[30];
 	int top = 0;
 	char array[100];
 	int i = 0;
 	int size = 0;
-	while(str[size] != '0')
+	while(str[size] != '\0')
 	{
 		++size;
 	}
-	while(str[i] != '0')
+	std::cout << size << " size " << std::endl;
+	while(str[i] != '\0')
 	{
-		array[i] = str[size-i];
+		array[i] = str[size-i-1];
+		std::cout << array[i] << " ";
+		++i;
 	}
 	char prefix[size];
-	for(int j = 0; j <= size; ++j)
-	{
+	for(int j = 0; j < size; ++j)
+	{	
+		
 		switch(array[j])
 		{
 			case '+':
 			if ( stack[top] == '-' || stack[top] == '+' )
 				{
-					stack[++top] = '+';
+					stack[++top] = array[j];
 				}
 			if ( stack[top] == '*' || stack[top] == '/' || stack[top] == '^' || stack[top] == '(' || stack[top] == ')' )
 				{
@@ -46,10 +50,11 @@ void Prefix(string &str)
 						}
 				}
 			break;
+	
 			case '-':
-			  if ( stack[top] == '-' || stack[top] == '+' )
+			if ( stack[top] == '-' || stack[top] == '+' )
                                 {
-                                        stack[++top] = '-';
+                                        stack[++top] = array[j];
                                 }
                         if ( stack[top] == '*' || stack[top] == '/' || stack[top] == '^' || stack[top] == '(' || stack[top] == ')' )
                                 {
@@ -60,28 +65,83 @@ void Prefix(string &str)
                                                 }
                                 }
                         break;
+	
 			case '*':
-			if ( stack[top] == '+' || stack[top] == '-' )
+			if ( stack[top] == '+' || stack[top] == '-' ||  stack[top] == '*' || stack[top] == '/' )
 				{
-					stack[++top] == '*';
+					stack[++top] = array[j];
 				}
-			if ( stack[top] == '*' || stack[top] == '/')
+			if ( stack[top] == '^' )
+				{	while(stack[top] == '^' ) 
+						{
+						prefix[size-j] = stack[top--];
+						++j;
+						}
+					stack[++top] = '*';	
+				}        
+			if ( stack[top] == '(' )
 				{
-					stack[++top] == array[j];
-				}	
+					stack[++top] = array[j];
+				}
+			break;
+			
 			case '/':
+			if ( stack[top] == '+' || stack[top] == '-' ||  stack[top] == '*' || stack[top] == '/' )
+				{
+					stack[++top] = array[j];
+				}
+			 if ( stack[top] == '^' )
+                                {       while(stack[top] == '^' )
+                                                {
+                                                prefix[size-j] = stack[top--];
+                                                ++j;
+                                                }
+                                        stack[++top] = '/';
+                                }
+                        if ( stack[top] == '(' )
+                                {
+                                        stack[++top] = array[j];
+                                }
+                        break;
+
 			case '^':
+			if ( stack[top] == '+' || stack[top] == '-' ||  stack[top] == '*' || stack[top] == '/' || stack[top] == '^' )
+				{
+					stack[++top] = array[j];
+				}
+			if ( stack[top] == '(' )
+                                {
+                                        stack[++top] = array[j];
+                                }
+                        break;
+
 			case '(':
+			if ( stack[top] == '+' || stack[top] == '-' ||  stack[top] == '*' || stack[top] == '/' || stack[top] == '^' || stack[top] == '(' )
+				{
+					stack[++top] = '(';
+				}
+			break;
+			
 			case ')':
-			default :
+			 while( stack[top] != '(' ) 
+				 {
+					 prefix[size-j] = stack[top--];
+                                         ++j;
+                                 }
+                         --top;
+			break;
+			default:
 			prefix[size-j] = array[j];
 			break; 
 		}
 	}
-	
+	for(int k = 0; k < size; ++k)
+		{
+			std::cout << prefix[k];
+		}	
 }
 
-void Postfix(string &str)
+/*void Postfix(string &str)
 {
         char stack[30];
         char array[100];
@@ -112,4 +172,4 @@ void Postfix(string &str)
 
 	
 }
-
+*/

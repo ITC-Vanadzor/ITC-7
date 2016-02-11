1c9c868e9906199e1af12ffb5b1bf95b98aca4e5
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-02-11 13:23:12 +0400

    added binary tree

--- Edgar_Hakobyan/Homework/DataStructures/tree.cpp
+++ Edgar_Hakobyan/Homework/DataStructures/tree.cpp
@@ -1,71 +1,99 @@
 #include <iostream>
+#include <vector>
 
 struct node
 {
 	int data;
 	node* left;
 	node* right;
-	node(int d)
+	node(int d,node* l,node*r)
 	:data(d)
-	,left(NULL)
-	,right(NULL)
+	,left(l)
+	,right(r)
 	{}
 };
 
-struct tree
+void insert(node* &root,int x)
 {
-	node* root;
-	tree();
-	void insert(node* root,int x);
-//	void del(node* root,int x);
-//	void find(node* root, int x);
-	void print(node* root);
-	~tree();
-}; 
-
-tree::tree()
-	:root(NULL)
-{}
-
-void tree::insert(node* root,int x)
-{
-	if (NULL == root)
+	if (root == NULL)
 	{
-		root = new node(x);
-	//	root->data = x;
-	//	root->left = NULL;
-	//	root->right = NULL;
+		root = new node(x,NULL,NULL);
+		return;
 	}
 	
 	if ( x < root->data )
 	{
 		if (root->left == NULL)
 		{
-			root->left = new node(x);
-		//	root->left->left = NULL;
-		//	root->left->right = NULL;
+			root->left = new node(x,NULL,NULL);
 		}
 		else
 		{
 			insert(root->left,x);
 		}
 	}
-	else
+	else if ( x > root->data )
 	{
 		if(root->right == NULL)
 		{
-			root->right = new node(x);
-	//		root->right->right = NULL;
-	//		root->right->left = NULL;
+			root->right = new node(x,NULL,NULL);
 		}
 		else
 		{
 			insert(root->right,x);
 		}
 	}
+	else
+	{
+		std::cout << "aydpisi tvyal arden ka" << std::endl;
+	}
 }
 
-void tree::print(node* root)
+int find(node* &root,int x)
+{
+	if (x == root->data)
+	{
+		return x;
+	}
+	else if( x < root->data)
+	{
+		return find(root->left,x);
+	}
+	else
+	{
+		return find(root->right,x);
+	}
+}
+
+void del(node* &root, int x)
+{
+	if (x == root->data)
+    {
+       	if(root->left == NULL && root->right == NULL)
+		{
+			delete root;
+			root = NULL;
+		}
+		if(root->left != NULL && root->right == NULL)
+		{
+			root = root->left;
+		}
+		if(root->left == NULL && root->right != NULL)
+        {
+            root = root->right;	
+        }
+    }
+    else if( x < root->data)
+    {
+        del(root->left,x);
+    }
+    else
+    {
+        del(root->right,x);
+    }
+}
+
+void print(node* root)
 {
     if (root == NULL) return;
    	print(root->left);
@@ -73,12 +101,34 @@ void tree::print(node* root)
     print(root->right);
 }
 
+void binary(node* root)
+{
+	std::vector<int> x;
+	if (root == NULL) return;
+    binary(root->left);
+    x.push_back(root->data);
+    binary(root->right);
+	for(int i = 0; i < x.size()-1; ++i)
+	{
+		if(x[i] > x[i+1])
+		{
+			std::cout << "no binary" << std::endl;
+			return;
+		}
+	}
+	std::cout << "binary" << std::endl;
+}
 
 int main()
 {
-	tree* r;
-	r.insert(r,5);
-	r.insert(r,2);
-	r.print(r);
+	node* r;
+	insert(r,5);
+	insert(r,2);
+	insert(r,6);
+	insert(r,8);
+	binary(r);
+	print(r);
+	del(r,6);
+	print(r);
 	return 0;
 }

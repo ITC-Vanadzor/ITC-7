8b4c837d8875e8ffa161199ac606d1f6f55297d5
Ani121293 <ani_saghatelyan@mail.ru>
2016-02-23 03:59:01 +0400

    Added javascript files, which hide an sign in div, saves datas from registration

---
+++ Ani_Saghatelyan/Homework/html/login/javaScript/registration.js
@@ -0,0 +1,9 @@
+
+
+ user= [];
+function addToArray(){
+        user.push({"name":document.getElementById("addName").value , "email":document.getElementById("addEmail").value, "password":document.getElementById("addPassword").value});
+        console.log(user);
+       
+}
+
---
+++ Ani_Saghatelyan/Homework/html/login/javaScript/signIn.js
@@ -0,0 +1,11 @@
+
+// function, which shows crate account and hides sign in div
+function ShowHideSignInCreateAccount(){
+
+	document.getElementById("hideSignIn").remove('show');
+        document.getElementBtId("showForgotPassword").style.display="block";
+}
+
+
+
+

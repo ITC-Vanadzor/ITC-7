1edc509f32d60d6a00b41a55e148f3d34107d940
hasmikus@gmail.com <Hasmik>
2016-02-23 23:18:39 +0400

    Added regExp complete version

--- Hasmik_Kirakosyan/Homework/JavaScript/regExp/test.js
+++ Hasmik_Kirakosyan/Homework/JavaScript/regExp/test.js
@@ -6,12 +6,10 @@ alert ("protocol "+protocol[1]);
 var host = str.match(/:\/\/(.*):/);
 alert("host "+host[1]);
 
-var port = str.match(/:(.*)\/search/);
-str = port[1];
-port = str.match(/:(.*)/);
+var port = str.match(/:(?!\/)(.*)\/search/);
 alert("port "+port[1]);
 
-var var1 = str.match(/\?(.*)\&/);
+var var1 = str.match(/\?(.*)&/);
 alert(var1[1]);
-//var val1 = str.match(/=(.*)\&/);
-//alert("variable 1 : "+var1[1]+" = "+val[1]);
+var var2 = str.match(/&(.*)$/);
+alert(var2[1]);

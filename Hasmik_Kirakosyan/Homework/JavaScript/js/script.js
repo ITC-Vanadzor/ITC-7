e2bb1e058b7f6503060fe4b55460c5f602508828
hasmikus@gmail.com <Hasmik>
2016-02-24 20:38:12 +0400

    Added HDM project working version

--- Hasmik_Kirakosyan/Homework/JavaScript/js/script.js
+++ Hasmik_Kirakosyan/Homework/JavaScript/js/script.js
@@ -1,3 +1,9 @@
+var jsonArr={"userData":[
+		{"userEmail":"hasmikus@gmail.com","password":"passwordMine"},
+      {"userEmail":"anisaghatelyan@mail.ru", "password": "Anispassword"},
+      {"userEmail":"smbatsargsyan@mail.ru", "password": "Smbatspassword"}
+]}
+
 function showDiv(divId){
 
 		  document.getElementById("forgotPassDiv").style.display="none";
@@ -7,32 +13,27 @@ function showDiv(divId){
 		  document.getElementById(divId).style.display="block";
 }
 
-var jsonArr = '';	
-var obj = JSON.parse(jsonArr);
-var fileContents = System.IO.File.ReadAllLines('jsonArr.txt');
-
-
 // Check login email password
-var signInButton = document.getElementById('signIn');
-signInButton = function () {
 
-		  for (var i = 0; i < obj.userData.length; i++) {
-					 if(((obj.userData[i].userEmail == document.getElementById("emailLogin").value) 
-													 &&	  (obj.userData[i].password != document.getElementById("passLogin"))) ||
-										  ((obj.userData[i].userEmail != document.getElementById("emailLogin").value) 
-											&&	  (obj.userData[i].password == document.getElementById("passLogin"))) ) {
+function signInSite () {
+
+		  for (var i = 0; i < jsonArr.userData.length; i++) {
+					 if(((jsonArr.userData[i].userEmail == document.getElementById("emailLogin").value) 
+													 &&	  (jsonArr.userData[i].password != document.getElementById("passLogin"))) ||
+										  ((jsonArr.userData[i].userEmail != document.getElementById("emailLogin").value) 
+											&&	  (jsonArr.userData[i].password == document.getElementById("passLogin"))) ) {
 								alert("Wrong login or password");
-								showDiv("signInDiv");
+								showDiv("forgotPassDiv");
 					 }
 		  }
 
 }
 
 // Check registration email
-var signUpButton = document.getElementById('signUp')		 
-signUpButton = function () {
-		  for (var i = 0; i < obj.userData.length; i++) {
-					 if(obj.userData[i].userEmail == document.getElementById("emailReg").value) {
+
+function  signUp() {
+		  for (var i = 0; i < jsonArr.userData.length; i++) {
+					 if(jsonArr.userData[i].userEmail == document.getElementById("emailReg").value) {
 								document.getElementById("validateEmail").style.color="red";
 								document.getElementById("validateEmail").innerHTML="There is a user  already with this email address. Try another";
 								t = true;	
@@ -41,11 +42,18 @@ signUpButton = function () {
 
 
 		  if(t) {
-					 obj['userData'].push({"userEmail":document.getElementById("email").value,"password":document.getElementById("password").value});
+					 jsonArr['userData'].push({"userEmail":document.getElementById("email").value,"password":document.getElementById("password").value});
 					 jsonArr = JSON.stringify(obj);
 		  }
 
 }
 
+// Send email to reset password in forgot password page
+function sendMail() {
+		      var link = "mailto:"+ document.getElementById("emailTo")
+							+ "&subject=" + escape("Reset password")
+			            + "&body=Hi,\n please reset enter new email";
 
+				window.location.href = link;
+}
 

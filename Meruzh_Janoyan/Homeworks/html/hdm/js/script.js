b1e8e3d722052801604fef1c2d1e3aa58162c843
Janoyan <meruzh2008@rambler.ru>
2016-02-25 00:37:02 +0400

    try username->Janoyan pass->student

--- Meruzh_Janoyan/Homeworks/html/hdm/js/script.js
+++ Meruzh_Janoyan/Homeworks/html/hdm/js/script.js
@@ -1,10 +1,55 @@
 
+function showDiv(divId) {
 
-function showDiv(divId){
+    document.getElementById("forgotPassDiv").style.display = "none";
+    document.getElementById("signInDiv").style.display = "none";
+    document.getElementById("signUpDiv").style.display = "none";
 
-  document.getElementById("forgotPassDiv").style.display="none";
-  document.getElementById("signInDiv").style.display="none";
-  document.getElementById("signUpDiv").style.display="none";
-	
-  document.getElementById(divId).style.display="block";
+    if (typeof divId === 'undefined') {
+        divId = location.href.match('[?]..(.+?)$');
+        if (divId) {
+            divId = divId[1];
+        }
+    }
+
+    if (!divId) {
+        divId = 'signInDiv';
+    }
+
+    switch (divId) {
+        case 'signUpDiv':
+            document.title = "Create account";
+            break;
+        case 'forgotPassDiv':
+            document.title = "Forgot password";
+            break;
+        default:
+            document.title = "Sign In to HDM";
+            divId='signInDiv';
+            if(signIn()) {return;}
+            
+    }
+    document.getElementById(divId).style.display = "block";
 }
+
+
+
+function signIn(){
+  
+    var email=location.href.match('email=(.+?)&');
+    var pass=location.href.match('password=(.+?)#');
+    
+    
+    if(email && pass){
+      if(email[1]=="Janoyan" && pass[1]=="student"){
+        location.href='index.html';
+        return 1;
+      }
+    }
+    
+    return 0;
+
+}
+
+
+

b72de40fbaf0b774a7fa7d7097fe60e3153afe3b
Araks <araksyahambaryan@mail.ru>
2016-02-25 01:04:09 +0400

    update fb_sdk

--- Araksya_Hambaryan/Homework/Web/FB_SDK/fb.js
+++ Araksya_Hambaryan/Homework/Web/FB_SDK/fb.js
@@ -6,3 +6,33 @@
 	      fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+function loginUser() {    
+    FB.login(function(response) {
+
+	    if (response.authResponse) {
+	    console.log('Welcome!  Fetching your information.... ');
+	    //console.log(response); // dump complete info
+	    access_token = response.authResponse.accessToken; //get access token
+	    user_id = response.authResponse.userID; //get FB UID
+
+	    FB.api('/me', function(response) {
+		user_email = response.email; //get user email
+		// you can store this data into your database             
+		});
+
+	    } else {
+	    //user hit cancel button
+	    console.log('User cancelled login or did not fully authorize.');
+
+	    }
+	    }, {
+scope: 'publish_stream,email'
+});
+    /*FB.login(
+	    function(response) {
+	    getStatus(); //This is a call to a different function...
+	    }, 
+	    {scope:'user_likes, offline_access'}
+	    );   */  
+}
+

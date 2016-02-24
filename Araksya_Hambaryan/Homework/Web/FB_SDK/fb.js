ee91aeda7df8da69fad8b3641bbc14606686464f
Araks <araksyahambaryan@mail.ru>
2016-02-25 02:19:26 +0400

    added finaly version of FB_SDK

--- Araksya_Hambaryan/Homework/Web/FB_SDK/fb.js
+++ Araksya_Hambaryan/Homework/Web/FB_SDK/fb.js
@@ -1,38 +1,46 @@
-(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	    js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.5&appId=1573877892928843";
-	      fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+window.fbAsyncInit = function() {
+    FB.init({
+	appId      : '1573877892928843',
+	xfbml      : true,
+	version    : 'v2.5'
+    });
 
-function loginUser() {    
+FB.getLoginStatus(function(response) {
+	if (response.status === 'connected') {
+	document.getElementById('login').style.visibility = 'hidden';
+	} else if (response.status === 'not_authorized') {
+	document.getElementById('status').innerHTML = 'We are not logged in.'
+	} else {
+	document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+	}
+	});
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+// login with facebook with extra permissions
+function login() {
     FB.login(function(response) {
-
-	    if (response.authResponse) {
-	    console.log('Welcome!  Fetching your information.... ');
-	    //console.log(response); // dump complete info
-	    access_token = response.authResponse.accessToken; //get access token
-	    user_id = response.authResponse.userID; //get FB UID
-
-	    FB.api('/me', function(response) {
-		user_email = response.email; //get user email
-		// you can store this data into your database             
-		});
-
+	    if (response.status === 'connected') {
+	    document.getElementById('status').innerHTML = 'We are connected.';
+	    document.getElementById('login').style.visibility = 'hidden';
+	    } else if (response.status === 'not_authorized') {
+	    document.getElementById('status').innerHTML = 'We are not logged in.'
 	    } else {
-	    //user hit cancel button
-	    console.log('User cancelled login or did not fully authorize.');
-
+	    document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
 	    }
-	    }, {
-scope: 'publish_stream,email'
-});
-    /*FB.login(
-	    function(response) {
-	    getStatus(); //This is a call to a different function...
-	    }, 
-	    {scope:'user_likes, offline_access'}
-	    );   */  
+	    }, {scope: 'email'});
 }
 
+// getting basic user info
+function getInfo() {
+    FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id'}, function(response) {
+	    document.getElementById('status').innerHTML = response.id;
+	    });
+}

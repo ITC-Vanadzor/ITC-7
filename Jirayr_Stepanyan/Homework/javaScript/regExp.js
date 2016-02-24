b40ac868fafdca18e0da3bde4424e3f2aa5a6694
v-levonyan <v_levonyan@yahoo.com>
2016-02-24 17:03:29 +0400

    edited regExp.js

--- Jirayr_Stepanyan/Homework/javaScript/regExp.js
+++ Jirayr_Stepanyan/Homework/javaScript/regExp.js
@@ -1,15 +1,28 @@
-var str = "http://google.com:8080/search?username=hi&password=by"
+var parser={
+    
+    str: "http://google.com:8080/search?username=hi&password=by",
+    "protocol":function(){return this.str.match(/[a-z]+(?=:\/\/)/i);},
 
-var protocol = str.match(/^(.*):\/\//);
-alert ("protocol: " +protocol[1]);
+     host :function() {
+	    return this.str.match(/[a-z]+[.]+[a-z]+(?=:)/i);
+     },
+    port : function() {
+	    return this.str.match(/[0-9]+(?=\/)/i);
+	    },
+    endpoint: function() {
+	    return this.str.match(/(?:)+[a-z]+(?=\?)/i);
+	    },
+    parameter1:function() {
+	    return this.str.match(/(?:username)+(?:=)+[a-z]+/i);
+},
 
-var host = str.match(/:\/\/(.*):/);
-alert("host: " +host[1]);
-
-var port = str.match(/:(?!\/)(.*)\/search/);
-alert("port: " +port[1]);
-
-var var1 = str.match(/\?(.*)&/);
-alert(var1[1]);
-var var2 = str.match(/&(.*)$/);
-alert(var2[1]);
+    parameter2:function() {
+	    return this.str.match(/(?:password)+(?:=)+[a-z]+/i);
+}
+}
+alert(parser.protocol());
+alert(parser.host());
+alert(parser.port());
+alert(parser.endpoint());
+alert(parser.parameter1());
+alert(parser.parameter2());

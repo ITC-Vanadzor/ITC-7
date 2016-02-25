aced8e362c2f14429ca2aff73f8a0cbfb3d7a78e
student <student@student-MS-7641.(none)>
2016-02-25 18:14:33 +0400

    Added script.js

--- Hasmik_Kirakosyan/Homework/JavaScript/urlTable/js/script.js
+++ Hasmik_Kirakosyan/Homework/JavaScript/urlTable/js/script.js
@@ -32,36 +32,51 @@ function urlParser (url) {
 					 return (path==null)?'none':path[1];	
 		  }
 } 
+	var x = document.createElement("table");
+	x.setAttribute("id", "urlTable");
+	x.setAttribute("border", "3");
+	document.body.appendChild(x);
+
+	var tbl = document.getElementById('urlTable');
+	var tbdy = document.createElement('tbody');
+	var tr = document.createElement('tr');
+
+	        
+    	var th = document.createElement("th");
+    	var t = document.createTextNode("table");
+   	th.appendChild(t);
+	tbdy.appendChild(th);
+
+	tbl.appendChild(tbdy);
+    	document.body.appendChild(tbl);
 
 
 function createTable() {
 
-		  // get url from user input
-		  url = prompt("Type url address! ");
-		  var urlObj = new urlParser(url);
+	// get url from user input
+	url = prompt("Type url address! ");
+	var urlObj = new urlParser(url);
 
+	
 
-		  var tbl = document.getElementById('urlTable');
-		  var tbdy = document.createElement('tbody');
-		  var tr = document.createElement('tr');
-		  for (var j = 0; j < 5; j++) {
-					 var td = document.createElement('td');
-					 switch (j) {
-								case 0: td.appendChild(document.createTextNode(urlObj.getProtocol()));
-										  break;
-								case 1: td.appendChild(document.createTextNode(urlObj.getHost()));
-										  break;
-								case 2: td.appendChild(document.createTextNode(urlObj.getPort()));
-										  break;
-								case 3: td.appendChild(document.createTextNode(urlObj.getPath()));
-										  break;
-								case 4: td.appendChild(document.createTextNode(urlObj.getPar1() +'\n' + urlObj.getPar2()));
-										  break;
-								default:td.appendChild('hi');
-					 }
-					 tr.appendChild(td)
-		  }
-		  tbdy.appendChild(tr);
-		  tbl.appendChild(tbdy);
+	for (var j = 0; j < 5; j++) {
+		var td = document.createElement('td');
+		switch (j) {
+			case 0: td.appendChild(document.createTextNode(urlObj.getProtocol()));
+				break;
+			case 1: td.appendChild(document.createTextNode(urlObj.getHost()));
+				break;
+			case 2: td.appendChild(document.createTextNode(urlObj.getPort()));
+				break;
+			case 3: td.appendChild(document.createTextNode(urlObj.getPath()));
+				break;
+			case 4: td.appendChild(document.createTextNode(urlObj.getPar1() +'\n' + urlObj.getPar2()));
+				break;
+			default:td.appendChild('hi');
+		}
+		tr.appendChild(td)
+	} 
+	tbdy.appendChild(tr);
+	tbl.appendChild(tbdy);
 
 }

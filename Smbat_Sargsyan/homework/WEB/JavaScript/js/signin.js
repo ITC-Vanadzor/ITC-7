cb651bd0b8f08e0239886561d8d021f16f9d717a
Smbat888 <Sargsyanyan@mail.ru>
2016-02-26 22:41:22 +0400

    HDM project's table search and filter

--- Smbat_Sargsyan/homework/WEB/JavaScript/js/signin.js
+++ Smbat_Sargsyan/homework/WEB/JavaScript/js/signin.js
@@ -13,18 +13,20 @@ var objPeople = [
       }
 ]
 function clicked() {
-	var mail = document.getElementById('mail');
-	var pass = document.getElementById('pass');
-
+      if (document.getElementById('Signin')) {
+            location.reload(false)
+     }
+     var mail = document.getElementById('mail');
+     var pass = document.getElementById('pass');
      for(i=0;i<objPeople.length;i++) {
-	if(mail.value == objPeople[i].usermail && pass.value == objPeople[i].userpass) {
+	     if(mail.value == objPeople[i].usermail && pass.value == objPeople[i].userpass) {
 			window.alert("you are log in as " + mail.value);
 			document.getElementById("Signin").style.display = "none";
 			document.getElementById("profile").style.display = "block";
 			return;
 		
-	}
-		}
-                window.alert("incorrect mail orpassword,try again");
-                document.getElementById("forgot").style.display = "block";
+	     }
+     }
+     window.alert("incorrect mail orpassword,try again");
+     document.getElementById("forgot").style.display = "block";
 }

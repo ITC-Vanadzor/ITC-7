cb6739df1935534197944b7f3fc17ea7c89ac5b2
hasmikus@gmail.com <Hasmik>
2016-02-26 10:45:59 +0400

    Added statistics page with sorting by date and market

--- Hasmik_Kirakosyan/Homework/JavaScript/HDM/js/statistics.js
+++ Hasmik_Kirakosyan/Homework/JavaScript/HDM/js/statistics.js
@@ -15,14 +15,14 @@ var tr = document.createElement('tr');
 
 // *********** Custom Data objects array*****************
 var Datas=[
-	{date:"03/25/2015",market:"Sparapet", product:"bread", price:"500"},
-	{date:"05/17/2012",market:"Arevik",   product:"cake",  price:"1800"}, 
-	{date:"01/5/2005" ,market:"GrinPlaza",product:"soap",  price:"3645"},
-	{date:"09/13/2015",market:"Voskya",  product:"hat",    price:"8500"},
-	{date:"06/25/2015",market:"Vega",    product:"wash-machine", price:"4530"},
-	{date:"11/18/2015",market:"Proshyan",product:"wine",   price:"50050"},    
-	{date:"01/05/2015",market:"Evrika",  product:"coffee", price:"50350"},
-	{date:"12/13/2015",market:"Smayl",   product:"sweets", price:"2500"}
+	{date:"2015-25-03",market:"Sparapet", product:"bread", price:"500"},
+	{date:"2012-17-05",market:"Arevik",   product:"cake",  price:"1800"}, 
+	{date:"2005-05-01" ,market:"GrinPlaza",product:"soap",  price:"3645"},
+	{date:"2015-13-09",market:"Voskya",  product:"hat",    price:"8500"},
+	{date:"2015-25-06",market:"Vega",    product:"wash-machine", price:"4530"},
+	{date:"2015-18-11",market:"Proshyan",product:"wine",   price:"50050"},    
+	{date:"2015-05-01",market:"Evrika",  product:"coffee", price:"50350"},
+	{date:"2015-13-12",market:"Smayl",   product:"sweets", price:"2500"}
 ];
 
 for (var i=0; i < 4; i++) {
@@ -48,7 +48,17 @@ for (var i=0; i < 4; i++) {
 }
 
 
-function printArr()  {
+// Get array of values of any property in Datas object array
+function getFields(input, field) {
+		      var output = [];
+				    for (var i=0; i < input.length ; ++i)
+								        output.push(input[i][field]);
+					     return output;
+}
+
+
+// print ani array
+function printArr(Datas)  {
 		  for (obj in Datas) {
 		  var tr = document.createElement('tr');
 				 for( key in Datas[obj]) {
@@ -75,9 +85,54 @@ function printArr()  {
 	}
 }
 
-/*function sortByDate() {
-  Datas.sort(dynamicSort(["date","01/5/2005"], ["date","03/25/2015"] );
-  printArr();   
-}*/
+printArr(Datas);
 
+
+// Sort by date
+function sortByDate() {
+   for (i=0; i<Datas.length; i++) {
+		document.getElementById("urlTable").deleteRow(0);
+	}
+   var dateArr = getFields(Datas, "date");
+	dateArr.sort();
+	
+	var newDatas = [];	
+	var i = 0;
+	for (dt in dateArr) 
+			  for (obj in Datas) {
+						 if (dateArr[dt] == Datas[obj].date) {
+									newDatas[i] = Datas[obj];
+									i++;
+						 }
+	}
+	printArr(newDatas);
+}
+
+sortByDate();
+
+
+// Sort by market
+
+function sortByMarket() {
+
+ for (i=0; i<Datas.length; i++) {
+		document.getElementById("urlTable").deleteRow(0);
+	}
+   var marketArr = getFields(Datas, "market");
+	marketArr.sort();
+	
+	var newDatas = [];	
+	var i = 0;
+	for (dt in marketArr) 
+			  for (obj in Datas) {
+						 if (marketArr[dt] == Datas[obj].market) {
+									newDatas[i] = Datas[obj];
+									i++;
+						 }
+	}
+	printArr(newDatas);
+
+}
+   
+sortByMarket();
 

03930eabd7fcc82fc0a21b68a829c308fdb8d45a
Araks <araksyahambaryan@mail.ru>
2016-02-28 01:54:01 +0400

    Added incomplete version to get JSON-friendsList

--- Araksya_Hambaryan/Homework/Web/FB_SDK/fb.js
+++ Araksya_Hambaryan/Homework/Web/FB_SDK/fb.js
@@ -1,38 +1,49 @@
+countClick = 0;
 window.fbAsyncInit = function() {
     FB.init({
 	appId      : '1573877892928843',
 	xfbml      : true,
 	version    : 'v2.5'
-});
-
-    FB.getLoginStatus(function(response) {
-	if (response.status === 'connected') {
-
-	document.getElementById('login').style.visibility = 'hidden';
-	}
-	});
+    });
 };
 
 (function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
+    if (d.getElementById(id)) {
+	return;
+    }
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
 
-// login with facebook with extra permissions
-function login() {
-    FB.login(function(response) {
-	    if (response.status === 'connected') {
-	        document.getElementById('login').style.visibility = 'hidden';
-	    }
-	    }, {scope: 'email'});
+function choice() { 
+    if (countClick == 0) {
+	login();
+    } else {
+	loadFriendList();
+    }
 }
 
-// getting basic user info
-function getInfo() {
-    FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id'}, function(response) {
-	    document.getElementById('status').innerHTML = response.id;
-	    });
+function login() {
+    countClick=1;
+    FB.login(function(response) {
+	    if (response.status === 'connected') {
+		alert("Connected");
+		FB.api('me', 'GET' ,function(response){
+		    if (response && !response.error) {
+			var userInfo = document.getElementById('info');
+			FB.api('/me', function(response) {
+			document.getElementsByClassName('withGoogle')[0].style.visibility = 'hidden';
+			userInfo.innerHTML = '<img src="https://graph.facebook.com/' + response.id + '/picture">' + response.name;
+			document.getElementById("login").innerHTML="Get Friends List";
+			});
+		    }
+		});
+	    }
+    }, {scope: 'email, user_friends'});
+}
+
+function loadFriendList(){
+window.location="FriendsList.html";
 }

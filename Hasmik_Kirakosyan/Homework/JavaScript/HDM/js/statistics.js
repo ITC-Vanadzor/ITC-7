635cd9c26c26e5a57d73fdeb4f98c5db81ab9673
hasmikus@gmail.com <Hasmik>
2016-02-27 14:56:47 +0400

    Added adding and deleting rows from table in HDM statistics table example

--- Hasmik_Kirakosyan/Homework/JavaScript/HDM/js/statistics.js
+++ Hasmik_Kirakosyan/Homework/JavaScript/HDM/js/statistics.js
@@ -1,13 +1,9 @@
 // ************ Create Table ***************************
 
 var table = document.createElement("table");
-table.setAttribute("id", "urlTable");
-//table.setAttribute("border", "1pxi");
+table.setAttribute("id", "Table");
 var tZone = document.getElementById("tableZone");
 tZone.appendChild(table);
-
-document.getElementById("urlTable").style.color = "blue";
-document.getElementById("urlTable").style.margin = "auto";
 
 var tbdy = document.createElement('tbody');
 var tr = document.createElement('tr');
@@ -15,14 +11,14 @@ var tr = document.createElement('tr');
 
 // *********** Custom Data objects array*****************
 var Datas=[
-	{date:"2015-25-03",market:"Sparapet", product:"bread", price:"500"},
-	{date:"2012-17-05",market:"Arevik",   product:"cake",  price:"1800"}, 
-	{date:"2005-05-01" ,market:"GrinPlaza",product:"soap",  price:"3645"},
-	{date:"2015-13-09",market:"Voskya",  product:"hat",    price:"8500"},
-	{date:"2015-25-06",market:"Vega",    product:"wash-machine", price:"4530"},
-	{date:"2015-18-11",market:"Proshyan",product:"wine",   price:"50050"},    
-	{date:"2015-05-01",market:"Evrika",  product:"coffee", price:"50350"},
-	{date:"2015-13-12",market:"Smayl",   product:"sweets", price:"2500"}
+{date:"2015-25-03",market:"Sparapet", product:"bread", price:"500"},
+{date:"2012-17-05",market:"Arevik",   product:"cake",  price:"1800"}, 
+{date:"2005-05-01" ,market:"GrinPlaza",product:"soap",  price:"3645"},
+{date:"2015-13-09",market:"Voskya",  product:"hat",    price:"8500"},
+{date:"2015-25-06",market:"Vega",    product:"wash-machine", price:"4530"},
+{date:"2015-18-11",market:"Proshyan",product:"wine",   price:"50050"},    
+{date:"2015-05-01",market:"Evrika",  product:"coffee", price:"50350"},
+{date:"2015-13-12",market:"Smayl",   product:"sweets", price:"2500"}
 ];
 
 var fields=["Date", "Market", "Product", "Price"];
@@ -36,91 +32,58 @@ for (field in fields) {
 }
 
 
-// Get array of values of any property in Datas object array
-function getFields(input, field) {
-		      var output = [];
-				    for (var i=0; i < input.length ; ++i)
-								        output.push(input[i][field]);
-					     return output;
-}
+
 
 
 // print ani array
 function printArr(Datas)  {
 		  for (obj in Datas) {
-		  var tr = document.createElement('tr');
-				 for( key in Datas[obj]) {
-					 var td = document.createElement('td');
-					 var productObj = Datas[obj];
-	
-					 switch (key) {
+					 var tr = document.createElement('tr');
+					 for( key in Datas[obj]) {
+								var td = document.createElement('td');
+								var productObj = Datas[obj];
+								td.appendChild(document.createTextNode(productObj[key]));
+								tr.appendChild(td);
 
-								case "date": td.appendChild(document.createTextNode(productObj[key]));
-										  break;
-								case "market": td.appendChild(document.createTextNode(productObj[key]));
-										  break;
-								case "product": td.appendChild(document.createTextNode(productObj[key]));
-										  break;
-								case "price": td.appendChild(document.createTextNode(productObj[key]));
-										  break;
-								default:break;
 					 }
-					 tr.appendChild(td);
+					 // Delete row button 
+					 td = document.createElement('td');
+					 button = document.createElement("BUTTON");
+					 del = document.createTextNode("-");
+					 button.appendChild(del);
+					 td.style.borderRight="hidden";
+					 td.style.borderTop="hidden";
+					 td.style.borderBottom = "hidden";
+					 td.setAttribute("onclick", "deleteRow(this)");
+					 td.appendChild(button);
 
+					 tr.appendChild(td);
+					 tbdy.appendChild(tr);
+					 table.appendChild(tbdy);
 		  }
-		  		    tbdy.appendChild(tr);
-		          table.appendChild(tbdy);
-	}
 }
 
 printArr(Datas);
 
+function addRow () {
 
-// Sort by date
-function sortByDate() {
-   for (i=0; i<Datas.length; i++) {
-		document.getElementById("urlTable").deleteRow(0);
-	}
-   var dateArr = getFields(Datas, "date");
-	dateArr.sort();
-	
-	var newDatas = [];	
-	var i = 0;
-	for (dt in dateArr) 
-			  for (obj in Datas) {
-						 if (dateArr[dt] == Datas[obj].date) {
-									newDatas[i] = Datas[obj];
-									i++;
-						 }
-	}
-	printArr(newDatas);
+		  var tr = document.createElement('tr');
+		  prDate=document.getElementById("date").value;
+		  prMarket=document.getElementById("market").value;
+		  prProduct=document.getElementById("product").value;
+		  prPrice=document.getElementById("price").value;
+		  var productData={date: prDate, market:prMarket, product: prProduct, price: prPrice};
+		  for( key in productData) {
+					 var td = document.createElement('td');
+					 td.appendChild(document.createTextNode(productData[key]));
+					 tr.appendChild(td);
+
+		  }
+		  tbdy.appendChild(tr);
+		  table.appendChild(tbdy);					
 }
 
-sortByDate();
-
-
-// Sort by market
-
-function sortByMarket() {
-
- for (i=0; i<Datas.length; i++) {
-		document.getElementById("urlTable").deleteRow(0);
-	}
-   var marketArr = getFields(Datas, "market");
-	marketArr.sort();
-	
-	var newDatas = [];	
-	var i = 0;
-	for (dt in marketArr) 
-			  for (obj in Datas) {
-						 if (marketArr[dt] == Datas[obj].market) {
-									newDatas[i] = Datas[obj];
-									i++;
-						 }
-	}
-	printArr(newDatas);
-
+function deleteRow (row) {
+		  var i = row.parentNode.parentNode.rowIndex;
+		  document.getElementById("Table").deleteRow(i);
 }
-   
-//sortByMarket();
-

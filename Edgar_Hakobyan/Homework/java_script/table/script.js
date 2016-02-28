db906e9aceac4102df4e573cefa4b257292c29ff
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-02-28 15:50:09 +0400

    axyusaki verjnakan tarberak@

--- Edgar_Hakobyan/Homework/java_script/table/script.js
+++ Edgar_Hakobyan/Homework/java_script/table/script.js
@@ -1,8 +1,21 @@
 function getstr() {
 str=prompt("Please import URL-s if you stoped inputing insert -1");
 }
-function pars() { 
-//str="http://google.com:8080/search?username=hi&password=by",
+
+
+function removeRow(obj) {
+	var table  = document.getElementById('table');
+	var numtr = obj.parentNode.parentNode.rowIndex;
+	count = document.getElementById('table').rows.length;
+	if( count-numtr > 1) {
+		table.deleteRow(numtr+1);
+	}
+	else {
+		table.deleteRow(numtr);
+	}
+}
+
+function addRow(obj) {
 protocol=str.match(/[a-z]+(?=:\/\/)/i);
 host=str.match(/[a-z]+[.]+[a-z]+(?=:)/i);
 port=str.match(/[0-9]+(?=\/)/i);
@@ -10,7 +23,7 @@ endpoint=str.match(/(?:)+[a-z]+(?=\?)/i);
 parameter1=str.match(/\?(.*)&/);
 parameter2=str.match(/&(.*)$/);
 
-parser={
+ parser={
     'protocol':protocol,
     'host'    :host,
     'port' 	  :port,
@@ -18,15 +31,27 @@ parser={
     'parameter1':parameter1[1],
     'parameter2':parameter2[1],
 }
-}
-function addtr()
-{
-	var tr = document.createElement('tr');
-	for(var j in parser) {
-		var td = document.createElement('td');
-		td.innerHTML = parser[j];
-		tr.appendChild(td);
+	var table = document.getElementById('table');
+	var num = obj.parentNode.parentNode.rowIndex;
+	if (num === undefined){
+		row = table.insertRow(1);
 		}
-	document.getElementById('table').appendChild(tr);
+	else{
+		row = table.insertRow(num+1);
+	}
+	var j = 0;
+		for(var i in parser){
+			cell = row.insertCell(j);
+			cell.innerHTML = parser[i];
+			j++;
+		}
+	cell = row.insertCell(j);
+	var plus = document.createElement('button');
+	plus.innerHTML = "+";
+	cell.appendChild(plus)
+	plus.addEventListener('click', function(){addRow(this)});
+	var minus = document.createElement('button');
+	minus.innerHTML = '-';
+	cell.appendChild(minus);
+	minus.addEventListener('click', function(){removeRow(this)});
 }
-

fa7513a9716d3a6986f48daaee2512a22877aabc
jiro <jiro-19944@mail.ru>
2016-02-28 19:16:27 +0400

    edited regExp.html and regExpTable.js

--- Jirayr_Stepanyan/Homework/javaScript/regExpTable.js
+++ Jirayr_Stepanyan/Homework/javaScript/regExpTable.js
@@ -1,44 +1,47 @@
-str= "http://google.com:8080/search?username=hi&password=by";
-
-function valueProtocol()
+function getstr() 
 {
-    return str.match(/[a-z]+(?=:\/\/)/i);
-}
-function valueHost()
-{
-    return str.match(/[a-z]+[.]+[a-z]+(?=:)/i);
-}
-function valuePort()
-{
-    return str.match(/[0-9]+(?=\/)/i);
-}
-function valueEndpoint()
-{
-    return str.match(/(?:)+[a-z]+(?=\?)/i);
-}
-function valueParameter1()
-{
-    return str.match(/(?:username)+(?:=)+[a-z]+/i);
-}
-function valueParameter2()
-{
-    return str.match(/(?:password)+(?:=)+[a-z]+/i);
+    str = prompt("Please import URL-s if you stoped inputing insert -1");
 }
 
-var parser={
-    protocol  : valueProtocol(),
-    host      : valueHost(),
-    port      : valuePort(),
-    endpoint  : valueEndpoint(),
-    parameter1: valueParameter1(),
-    parameter2: valueParameter2()
+function removeTr(tr)
+{
+    tr.remove();
 }
-myProtocol = document.getElementById("protocol")
-consol.log(valueProtocol());
-/*alert(parser.protocol);
-alert(parser.host);
-alert(parser.port);
-alert(parser.endpoint);
-alert(parser.parameter1);
-alert(parser.parameter2);*/
 
+function pars() 
+{ 
+    //str="http://google.com:8080/search?username=hi&password=by",
+    protocol=str.match(/[a-z]+(?=:\/\/)/i);
+    host=str.match(/[a-z]+[.]+[a-z]+(?=:)/i);
+    port=str.match(/[0-9]+(?=\/)/i);
+    endpoint=str.match(/(?:)+[a-z]+(?=\?)/i);
+    parameter1=str.match(/\?(.*)&/);
+    parameter2=str.match(/&(.*)$/);
+
+parser = 
+{
+    'protocol'	:protocol,
+    'host'	:host,
+    'port'	:port,
+    'endpoint'	:endpoint,
+    'parameter1':parameter1[1],
+    'parameter2':parameter2[1],
+}
+
+    var tr = document.createElement('tr');
+    for(var j in parser) 
+    {
+	var td = document.createElement('td');
+	td.innerHTML = parser[j];
+	tr.appendChild(td);
+    }
+    document.getElementById('table').appendChild(tr);
+    var plus = document.createElement('button');
+    plus.innerHTML = "+";
+    tr.appendChild(plus)
+    plus.addEventListener('click', function(){getstr(tr)});
+    var minus = document.createElement('button');
+    minus.innerHTML = "-";
+    tr.appendChild(minus);
+    minus.addEventListener('click', function(){removeTr(tr)});
+}

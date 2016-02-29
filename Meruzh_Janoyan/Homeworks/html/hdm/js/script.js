c2fc5f135d84d2be5e61a40a16303e3a414c871c
Janoyan <meruzh2008@rambler.ru>
2016-02-29 23:02:50 +0400

    last commit 29_Feb 23:03

--- Meruzh_Janoyan/Homeworks/html/hdm/js/script.js
+++ Meruzh_Janoyan/Homeworks/html/hdm/js/script.js
@@ -64,7 +64,7 @@ function progressSteps(idProgress, idCircle, idStep) {
     document.getElementById("thirdCircle").style.background = "linear-gradient(to top, #0094F7, #007acc)";
     document.getElementById("secondCircle").style.background = "linear-gradient(to top, #0094F7, #007acc)";
     document.getElementById("firstCircle").style.background = "linear-gradient(to top, #0094F7, #007acc)";
-    document.getElementById(idCircle).style.background = "#faaa80";
+    document.getElementById(idCircle).style.background = "#999999";
 
     document.getElementById("thirdProgress").style.background = "#bfbfbf";
     document.getElementById("secondProgress").style.background = "#bfbfbf";
@@ -79,21 +79,17 @@ function progressSteps(idProgress, idCircle, idStep) {
             document.getElementById("firstProgress").style.background = "#007acc";
     }
 
-    //  document.getElementById(divId).style.display="block";
 }
 
 
 
 
-
-
-
-
 /*Database structure */
-function DatabaseItem(name, date, money) {
+function DatabaseItem(name, date, money,note) {
     this.name = name;
     this.date = date;
     this.money = money;
+    this.note=(note)?note:"";
     this.getArray = function() {
         var arr = [this.name, this.date.toLocaleDateString(), this.date.toLocaleTimeString(), this.money];
         return arr;
@@ -159,70 +155,69 @@ function Database() {
 
 }
 
-var myDb=new Database();
+var myDb = new Database();
 
-myDb.addItem("smile", new Date(2016, 8, 23,14,14),  2000);
-myDb.addItem("jyre", new Date(2016, 11, 2,23,32),  1000);
-myDb.addItem("jumper", new Date(2016, 4, 7,12,50), 2010);
-myDb.addItem("classile", new Date(2016, 4, 21,12,32),  5700);
-myDb.addItem("grand", new Date(2016, 7, 4,4,45),  4110);
-myDb.addItem("book", new Date(2016, 8, 9,12,10), 2200);
-myDb.addItem("mans", new Date(2016, 8, 23,10,30), 2100);
-myDb.addItem("jeny", new Date(2016, 9, 1,10,41), 5000);
-myDb.addItem("sumy", new Date(2016, 10, 9,15,00), 3000);
-myDb.addItem("star", new Date(2016, 3, 8,22,20),  1500);
+myDb.addItem("smile", new Date(2016, 8, 23, 14, 14), 2000);
+myDb.addItem("jyre", new Date(2016, 11, 2, 23, 32), 1000);
+myDb.addItem("jumper", new Date(2016, 4, 7, 12, 50), 2010);
+myDb.addItem("classile", new Date(2016, 4, 21, 12, 32), 5700);
+myDb.addItem("grand", new Date(2016, 7, 4, 4, 45), 4110);
+myDb.addItem("book", new Date(2016, 8, 9, 12, 10), 2200);
+myDb.addItem("mans", new Date(2016, 8, 23, 10, 30), 2100);
+myDb.addItem("jeny", new Date(2016, 9, 1, 10, 41), 5000);
+myDb.addItem("sumy", new Date(2016, 10, 9, 15, 00), 3000);
+myDb.addItem("star", new Date(2016, 3, 8, 22, 20), 1500);
 
 
 
 
 function showGraph() {
-    
-    var constX=50;
-    var constY=50;
-    var constK=60;
-    
-    document.getElementById('iGraph').style.display='block';
-    
-    myDb.sortByDate();//sorting by date
-    dateFrom=myDb.item[0].date;
-    dateTo=myDb.item[myDb.count-1].date;
-    var notOneYear=dateTo.getFullYear()-dateFrom.getFullYear();
-    var minYear=dateFrom.getFullYear();
-    
-   if(notOneYear){
-      notOneYear++;
-      notOneYear=(notOneYear>8)?8:notOneYear;
+
+    var constX = 50;
+    var constY = 50;
+    var constK = 60;
+
+    document.getElementById('iGraph').style.display = 'block';
+
+    myDb.sortByDate(); //sorting by date
+    dateFrom = myDb.item[0].date;
+    dateTo = myDb.item[myDb.count - 1].date;
+    var notOneYear = dateTo.getFullYear() - dateFrom.getFullYear();
+    var minYear = dateFrom.getFullYear();
+
+    if (notOneYear) {
+        notOneYear++;
+        notOneYear = (notOneYear > 8) ? 8 : notOneYear;
+    } else {
+        notOneYear = 1;
     }
-    else{
-    notOneYear=1;
-    }
-   
-   var uniArr=myDb.unionDays();//union repeated days
-      
-   
+
+    var uniArr = myDb.unionDays(); //union repeated days
+
+
     //geting canvas object
     var canvObj = document.getElementById("iGraph"),
-    ctx = iGraph.getContext('2d');
+        ctx = iGraph.getContext('2d');
     canvObj.width = 800;
     canvObj.height = 400;
-    
+
     //X and Y coordinates 
-    ctx.strokeStyle = 'green';    
-    ctx.fillRect(constX,constY,canvObj.width - 2*constX,canvObj.height - 2*constY);    
+    ctx.strokeStyle = 'green';
+    ctx.fillRect(constX, constY, canvObj.width - 2 * constX, canvObj.height - 2 * constY);
     ctx.beginPath();
     ctx.lineWidth = 0.5;
-    for(var i=constX;i<canvObj.width - constX;i+=7){
-      ctx.moveTo(i, constX);
-      ctx.lineTo(i, canvObj.height - constY);
-      
+    for (var i = constX; i < canvObj.width - constX; i += 7) {
+        ctx.moveTo(i, constX);
+        ctx.lineTo(i, canvObj.height - constY);
+
     }
-    for(var i=constY;i<canvObj.height - constY;i+=7){
-      ctx.moveTo(constX, i);
-      ctx.lineTo(canvObj.width - constX,i);
-      
+    for (var i = constY; i < canvObj.height - constY; i += 7) {
+        ctx.moveTo(constX, i);
+        ctx.lineTo(canvObj.width - constX, i);
+
     }
     ctx.stroke();
-    
+
     ctx.strokeStyle = 'blue';
     ctx.beginPath();
     ctx.lineWidth = 5;
@@ -230,30 +225,29 @@ function showGraph() {
     ctx.lineTo(constX, canvObj.height - constY);
     ctx.lineTo(canvObj.width - constX, canvObj.height - constY);
     ctx.stroke();
-    
+
 
     ctx.font = "15px Arial";
-    if(notOneYear>1){
-      
-      var counter=0;
-      var interval=700/notOneYear;
+    if (notOneYear > 1) {
 
-      for(i=dateFrom.getFullYear();counter<notOneYear;++i,counter++){
-          ctx.fillText(i, counter * interval+ constX, canvObj.height - (constY-20));
-      }
-      
-      }
-      else{
-      
-      var mnth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jly", "Aug", "Sep", "Oct", "Nov", "Dec"];      
-      for (i in mnth) {
-        ctx.fillText(mnth[i], i * 55 + constX, canvObj.height - (constY-20));
-      }
-    
+        var counter = 0;
+        var interval = 700 / notOneYear;
+
+        for (i = dateFrom.getFullYear(); counter < notOneYear; ++i, counter++) {
+            ctx.fillText(i, counter * interval + constX, canvObj.height - (constY - 20));
+        }
+
+    } else {
+
+        var mnth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jly", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        for (i in mnth) {
+            ctx.fillText(mnth[i], i * 55 + constX, canvObj.height - (constY - 20));
+        }
+
     }
-    
-    
-    
+
+
+
     var money = [1000, 2000, 3000, 4000, 5000, 6000];
     for (i in money) {
         ctx.fillText(money[i], 10, (canvObj.height - 50) - (i * 55));
@@ -266,71 +260,56 @@ function showGraph() {
 
     ctx.strokeStyle = grd;
     ctx.beginPath();
-    for (var i=0;i<uniArr.count;++i) {
-        var intervalYear=uniArr.item[i].date.getFullYear();
-        intervalYear-=minYear;
-        intervalYear=700/notOneYear*intervalYear; 
-        interval=60/notOneYear;
-        
-        x = intervalYear+ (uniArr.item[i].date.getMonth() ) * interval+55 + uniArr.item[i].date.getDate();
+    for (var i = 0; i < uniArr.count; ++i) {
+        var intervalYear = uniArr.item[i].date.getFullYear();
+        intervalYear -= minYear;
+        intervalYear = 700 / notOneYear * intervalYear;
+        interval = 60 / notOneYear;
+
+        x = intervalYear + (uniArr.item[i].date.getMonth()) * interval + 55 + uniArr.item[i].date.getDate();
         y = uniArr.item[i].money * (canvObj.height - 50) / 6000;
         y = canvObj.height - y;
-        if(i){
-        ctx.lineTo(x, y);
-        ctx.moveTo(x, y);
-        }
-        else{
+        if (i) {
+            ctx.lineTo(x, y);
+            ctx.moveTo(x, y);
+        } else {
             ctx.moveTo(x, y);
         }
         ctx.stroke();
-        
+
     }
-    
+
 }
 
 
 
 
+function table() {
+    myDb.sortByDate();
+    var myTable = document.getElementById("myTable");
+    myTable.setAttribute("style", "width:50% ; margin:5% 25% 0%; padding:0%;");
+    var header = myTable.createTHead();
+    var headerRow = header.insertRow(0);
+    var Head = ["Market", "Date", "Time", "Price"];
+    for (var i = 0; i < Head.length; ++i) {
+        var dataDate = headerRow.insertCell(i);
+        dataDate.innerHTML = Head[i];
+        dataDate.setAttribute("style", "font-weight: bold;");
 
+    }
+    for (var i = 0; i < myDb.count; i++) {
+        tmp = myDb.item[i].getArray();
+        var row = document.createElement("tr");
+        for (s in tmp) {
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode(tmp[s]);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
 
+        myTable.appendChild(row);
+    }
 
-
-
-
-
-
-
-function  table()
-{
-	   	myDb.sortByDate();
-  		var myTable = document.getElementById("myTable");
-		myTable.setAttribute("style", "width:50% ; margin:5% 25% 0%; padding:0%;");
-	        var header = myTable.createTHead();
-		var headerRow = header.insertRow(0);
-		var Head=["Market","Date","Time","Price"] ;
-		for(var i=0; i< Head.length;++i)
-		{
-			var dataDate = headerRow.insertCell(i);
-			dataDate.innerHTML=Head[i];
-			dataDate.setAttribute("style", "font-weight: bold;");
-
-		}
-			for (var i = 0; i < myDb.count; i++) 
-	    		{
-                tmp=myDb.item[i].getArray();
-                var row = document.createElement("tr");
-                for(s in tmp){
-      					var cell = document.createElement("td");
-      					var cellText = document.createTextNode(tmp[s]);
-      					cell.appendChild(cellText);
-      					row.appendChild(cell);
-                }
-
-    				myTable.appendChild(row);
-  			}
- 
-  			myTable.setAttribute("border", "2");
-        showGraph();
- }	
-
-
+    myTable.setAttribute("border", "2");
+    showGraph();
+}

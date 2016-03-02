f172802a5879fdf6a786bc71847584e78057e32f
hasmikus@gmail.com <Hasmik>
2016-03-02 23:12:31 +0400

    Added controllser file

--- Hasmik_Kirakosyan/Homework/NodeJs/hdm/modules/controller.js
+++ Hasmik_Kirakosyan/Homework/NodeJs/hdm/modules/controller.js
@@ -1,13 +1,16 @@
 'use strict';
 
+var express = require('express'),
+	     app  = express();
+var bodyParser = require('body-parser');
 var util = require('util');
 var fs = require('fs');
 var json = require('./data');
 var jsonFile = './data.json';
 
 module.exports.signUp = function(req, res) {
-	json.post.push(req.body);
-	var body = JSON.stringify(log);
+		  json.post.push(req.body);
+  		  var body = JSON.stringify(json);
 	fs.writeFile(jsonFile, body, function(e,s) {
 			  console.log(e,s, '----');
 	});

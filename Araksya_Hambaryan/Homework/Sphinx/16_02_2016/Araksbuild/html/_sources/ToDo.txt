935f5da49694395f62c4e189237e3c380f3061c4
Smbat Sargsyan <Sargsyanyan@mail.ru>
2016-03-02 14:26:08 +0400

    test

---
+++ Araksya_Hambaryan/Homework/Sphinx/16_02_2016/Araksbuild/html/_sources/ToDo.txt
@@ -0,0 +1,11 @@
+**ToDo**
+========
+
+.. todo::
+   Complet Homework.rst
+
+.. todo::
+   Complet InFuture.rst
+
+.. todo::
+   Add Summary
--- Smbat_Sargsyan/homework/WEB/JavaScript/js/signin.js
+++ Smbat_Sargsyan/homework/WEB/JavaScript/js/signin.js
@@ -16,22 +16,16 @@ function clicked() {
 	var mail = document.getElementById('mail');
 	var pass = document.getElementById('pass');
 
+
      for(i=0;i<objPeople.length;i++) {
-	if(mail.value == objPeople[i].usermail) {
-		if(pass.value == objPeople[i].userpass) {
+	if(mail.value == objPeople[i].usermail && pass.value == objPeople[i].userpass) {
 			window.alert("you are log in as " + mail.value);
 			document.getElementById("Signin").style.display = "none";
-			document.getElementById("registration").style.display = "none";	
-			document.getElementById("forgot").style.display = "none";	
 			document.getElementById("profile").style.display = "block";
 			return;
 		}
 	}
 		
-      }
-                window.alert("incorrect mail orpassword,try again");
-                document.getElementById("Signin").style.display = "none";
-                document.getElementById("forgot").style.display = "block";
-                document.getElementById("registration").style.display = "none";
-                document.getElementById("profile").style.display = "none";
+                window.alert("incorrect mail or password,try again");
+		document.getElementById("forgot").style.display = "block";
 }

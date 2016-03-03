47af41d16c2fad850b6fe04d5b16f9872d3f5006
Ani121293 <ani_saghatelyan@mail.ru>
2016-03-04 02:10:18 +0400

    Added some logic for sign-in and sign-up, changed the location of logfile

--- Ani_Saghatelyan/Homework/nodeJs/nodejs/config/index.js
+++ Ani_Saghatelyan/Homework/nodeJs/nodejs/config/index.js
@@ -3,11 +3,13 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var multer = require('multer');
-
+var path=require('path');
 module.exports = function(server) {
     // html renderer
     server.set('view engine', 'ejs');
     server.engine('html', require('ejs').renderFile);
+    //sets view for rendering html from public directory
+     server.set('views',path.join(__dirname, '../', 'public'));
     // for rendering static file
     server.use(express.static('./public'));
 

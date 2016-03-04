a4f59b3afb0ea988e40fce975ce982ecd908bbd8
hasmikus@gmail.com <Hasmik>
2016-03-04 23:08:08 +0400

    Added corrected version of hdm project

--- Hasmik_Kirakosyan/Homework/NodeJs/hdm/modules/user/controller.js
+++ Hasmik_Kirakosyan/Homework/NodeJs/hdm/modules/user/controller.js
@@ -10,8 +10,7 @@ module.exports.get = function(req, res) {
 };
 
 module.exports.signUp = function(req, res) {
-	var obj = require(__dirname+"/data.json");
-	var arr = obj.post;
+	var arr = json.post;
 	for (var i = 0; i<arr.length; ++i ) {
 		if (arr[i].email === req.body.email ) {
 		res.end("Sorry, there is an user with this email address!"); 		
@@ -22,17 +21,17 @@ module.exports.signUp = function(req, res) {
 	var body = JSON.stringify(json);
 	console.log(body);
 	fs.writeFile(jsonFile, body, function(e,s) {});
-	res.end();
+  	res.render("src/profile2.html");
 };
 
 module.exports.signIn = function(req, res) {
 	var email = req.body.email;
 	var password = req.body.password;
-	var obj = require(__dirname+"/data.json");
-	var arr = obj.post;
+	var arr = json.post;
 	for (var i = 0; i<arr.length; ++i ) {
 		if (arr[i].email === email && arr[i].password === password) {
 		res.end("Ok, you are siggned in"); 		
+		return;
 		}
 	}
 

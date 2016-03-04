d0a1180beb2b8b846d2a56451dc3cee19ed10bc6
Ani121293 <ani_saghatelyan@mail.ru>
2016-03-05 01:58:49 +0400

    var async=require('async'); var  fs=require('fs'); // array for saving filenames var files=[]; // var name;
    // creates and writes  the word 'hello' in all files
    function write(filename, index) {
      fs.writeFile(filename, 'Hello'+filename ,function(error,data){
    		if(error){
    			throw error;
    		}
            console.log('write err '+filename+error,'write succ '+filename+data)});
    
    }
    // 5 times calls wrire function and pushes the names of files into files array
    for (var i=0; i<5; ++i)
    {
     	write(i+'.txt',i);
    	files.push(i+'.txt');
    }
    // appends in a file '+++'
    var append = function (name,callback)
    {
    
    	fs.appendFile(name, '+++', 'utf8', function(error, data){
                            //error=true;
    			//if(error){ callback(error);}
                            callback(error, data);
                            console.log(" err appendFile "+name+' ' +error, " succ appendFile "+name+' '+ data)});
    }
    // calls append function for all elements of the array, then after it calls callback function
    async.map(files, append, function(error, data){
              console.log("  map "+error, "succ mapp "+ data);
    	  });

--- Ani_Saghatelyan/Homework/nodeJs/map.async/map.js
+++ Ani_Saghatelyan/Homework/nodeJs/map.async/map.js
@@ -6,10 +6,13 @@ var files=[];
 var name;
 
 // creates and writes  the word 'hello' in all files
-function write(filename, index,callback) {
+function write(filename, index) {
   fs.writeFile(filename, 'Hello'+filename ,function(error,data){
+		if(error){
+			throw error;
+		}
         console.log('write err '+filename+error,'write succ '+filename+data)});
-       // callback(error, data);
+ 
 } 
 // 5 times calls wrire function and pushes the names of files into files array
 for (var i=0; i<5; ++i)
@@ -22,12 +25,12 @@ var append = function (name,callback)
 {
 
 	fs.appendFile(name, '+++', 'utf8', function(error, data){
-                
-                      //  callback(error, data);	
+                        //error=true;
+			//if(error){ callback(error);}
+                        callback(error, data);	
                         console.log(" err appendFile "+name+' ' +error, " succ appendFile "+name+' '+ data)});	
 }
 // calls append function for all elements of the array, then after it calls callback function
-async.map(files, append, function(error, data){
-          // callback(error, data);
+async.map(files, append, function(error, data){       
           console.log("  map "+error, "succ mapp "+ data);
-	  })
+	  });

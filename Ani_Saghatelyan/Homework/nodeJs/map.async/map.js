5a75593fb3c6354878bfb5d5ad1bba128669b6f3
Ani121293 <ani_saghatelyan@mail.ru>
2016-03-05 01:07:24 +0400

    Added incorrect version

--- Ani_Saghatelyan/Homework/nodeJs/map.async/map.js
+++ Ani_Saghatelyan/Homework/nodeJs/map.async/map.js
@@ -1,21 +1,33 @@
 var async=require('async');
 var  fs=require('fs');
+// array for saving filenames
 var files=[];
+// 
 var name;
+
+// creates and writes  the word 'hello' in all files
+function write(filename, index,callback) {
+  fs.writeFile(filename, 'Hello'+filename ,function(error,data){
+        console.log('write err '+filename+error,'write succ '+filename+data)});
+       // callback(error, data);
+} 
+// 5 times calls wrire function and pushes the names of files into files array
 for (var i=0; i<5; ++i)
 {
-	fs.writeFile( i+'.txt', 'Hello'+i , 'utf8', function(err,succ){
-		callback("errRead "+err,"succRead "+succ)});
-
+ 	write(i+'.txt',i);
 	files.push(i+'.txt');
 }
-
-var append = function (name)
+// appends in a file '+++'
+var append = function (name,callback)
 {
-	fs.appendFile(name, '+++', 'utf8', function(err, succ){
-			callback("err appendFile "+err, "succ appendFile "+ succ)})	
+
+	fs.appendFile(name, '+++', 'utf8', function(error, data){
+                
+                      //  callback(error, data);	
+                        console.log(" err appendFile "+name+' ' +error, " succ appendFile "+name+' '+ data)});	
 }
-
-async.map(files, append, function(err, succ){
-	  callback("err  map "+err, "succ mapp "+ succ)});	
-
+// calls append function for all elements of the array, then after it calls callback function
+async.map(files, append, function(error, data){
+          // callback(error, data);
+          console.log("  map "+error, "succ mapp "+ data);
+	  })

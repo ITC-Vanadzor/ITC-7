575f487a71c8eef07093a7dc9518458dd423e3b1
hasmikus@gmail.com <Hasmik>
2016-03-04 20:56:18 +0400

    Deleted some unneseccary files added replaced ones

---
+++ Hasmik_Kirakosyan/Homework/NodeJs/async/controller.js
@@ -0,0 +1,19 @@
+'use strict';
+var fs = require('fs');
+var async = require('async');
+
+var obj = {};
+async.series([
+					 function(callback){
+								obj.color = "yellow";
+								callback(null, obj);
+					 },
+					 function(callback){
+								obj.lives = 5;
+								callback(null, obj);
+					 }
+],
+// optional callback
+function(err, results){
+		  console.log('error : ', error, 'results : ', results);
+});

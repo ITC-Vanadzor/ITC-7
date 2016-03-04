d14031c7e6b5ab2a213eedd0d609c32244ffb647
Hasmikus <hasmikus@gmail.com>
2016-03-04 15:32:26 +0400

    some changes

--- Ani_Saghatelyan/Homework/nodeJs/map.async/map.js
+++ Ani_Saghatelyan/Homework/nodeJs/map.async/map.js
@@ -1,20 +1,21 @@
 var async=require('async');
 var  fs=require('fs');
 var files=[];
+var name;
 for (var i=0; i<5; ++i)
 {
-	var file=fs.writeFile( i+'.txt', 'Hello'+i , 'utf8', function(err,succ){
-	
-			console.log("err "+err,"succ "+succ)})
-	files.push(file);
+	fs.writeFile( i+'.txt', 'Hello'+i , 'utf8', function(err,succ){
+		callback("errRead "+err,"succRead "+succ)});
+
+	files.push(i+'.txt');
 }
 
 var append = function (name)
 {
 	fs.appendFile(name, '+++', 'utf8', function(err, succ){
-			console.log("err appendFile " +err, "succ appendFile "+ succ)})	
+			callback("err appendFile "+err, "succ appendFile "+ succ)})	
 }
 
 async.map(files, append, function(err, succ){
-	  console.log("err  map "+ err, "succ mapp "+ succ)});	
+	  callback("err  map "+err, "succ mapp "+ succ)});	
 

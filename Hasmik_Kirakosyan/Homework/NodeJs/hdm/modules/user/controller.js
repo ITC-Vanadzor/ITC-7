af31501aa14d34628b40445c2e38857542de36b0
hasmikus@gmail.com <Hasmik>
2016-03-08 22:45:41 +0400

    Added signUp and signIn mysql

--- Hasmik_Kirakosyan/Homework/NodeJs/hdm/modules/user/controller.js
+++ Hasmik_Kirakosyan/Homework/NodeJs/hdm/modules/user/controller.js
@@ -1,39 +1,35 @@
 'use strict';
 
-var fs = require('fs');
-var json = require('./data');
-var jsonFile = __dirname+'/data.json';
+var db = require('./db');
 
 module.exports.get = function(req, res) {
-	res.end("-----Index-----");
+		  res.end("-----Index-----");
 
 };
 
-module.exports.signUp = function(req, res) {
-	var arr = json.post;
-	for (var i = 0; i<arr.length; ++i ) {
-		if (arr[i].email === req.body.email ) {
-		res.end("Sorry, there is an user with this email address!"); 		
-		return;
-		}
-	}
-   json.post.push(req.body);
-	var body = JSON.stringify(json);
-	console.log(body);
-	fs.writeFile(jsonFile, body, function(e,s) {});
-  	res.render("src/profile2.html");
-};
+module.exports.signUP = function(req, res) {
+	  db.query('SELECT * from usersData where email="'+ req.body.email +'" and password = "' +req.body.password +'"', function(err, rows) {
+					 if (rows[0]) {
+								res.end("There is a user with this email address"); 		
+								return;
+					 }
+					 else {
+								db.query('Insert INTO usersData set username ="' +req.body.username+ '", email ="' + req.body.email + '",password="' +req.body.password +'"', function(err, success) {
+										  console.log(err, success);
+										  res.render('src/profile2.html');
+							});
+					}	
+			});
+}
 
 module.exports.signIn = function(req, res) {
-	var email = req.body.email;
-	var password = req.body.password;
-	var arr = json.post;
-	for (var i = 0; i<arr.length; ++i ) {
-		if (arr[i].email === email && arr[i].password === password) {
-		res.end("Ok, you are siggned in"); 		
-		return;
-		}
-	}
-
-	res.end("No such user.  Please sign in");
+	  db.query('SELECT * from usersData where email="'+ req.body.email +'" and password = "' +req.body.password +'"', function(err, rows) {
+					 if (rows[0]) {
+								res.end("src/profile2.html"); 		
+								return;
+					 }
+					 else {
+								res.end("No such user!!!");
+					 }
+	});
 };

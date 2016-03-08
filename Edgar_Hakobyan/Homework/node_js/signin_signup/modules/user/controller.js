87f27cad82315cefc1b6bb53ba49ecdd3b0237f9
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-03-08 21:37:17 +0400

    added nodejs mysql db

--- Edgar_Hakobyan/Homework/node_js/signin_signup/modules/user/controller.js
+++ Edgar_Hakobyan/Homework/node_js/signin_signup/modules/user/controller.js
@@ -1,11 +1,14 @@
 'use strict';
-var fs = require('fs');
 var express = require('express');
 var path = require('path');
-var log = require('./log');
-var logFile = './log.json';
-
-
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'hdmDb'
+});
+connection.connect();
 module.exports.get = function(req, res) {
     res.end('-----index-----');
 };
@@ -16,38 +19,29 @@ module.exports.id = function(req, res){
 
 
 module.exports.registration = function(req, res){
-    log.push(req.body);
-    var body = JSON.stringify(req.body);
-    console.log(body);
-    fs.writeFile(logFile, body, function(e, s){
-      console.log(e, s, '---registration--');
-    });
+	var username = req.body.username;
+	var email = req.body.email;
+    var password = req.body.password;
+	
+var hdmDb = {
+  username: username,
+  email: email,
+  password: password
+};
     res.end('------post------');
 };
 
 module.exports.signin = function(req,res) {
     var email = req.body.email;
     var password = req.body.password;
-    fs.readFile(logFile,'utf8',function(err,data) {
-	var jsonData = JSON.parse(data);
-		console.log(jsonData);
-//	var jsonData = JSON.stringify(jsonData1.email);
-//	for(var i  in jsonData) {
-//					console.log(email);	
-//				console.log(jsonData[0].email);
-//					console.log(password);	
-//				console.log(jsonData[0].password);
-
-           if(email == jsonData.email && password == jsonData.password) {
-		res.sendfile('public/profile.html');
-	        console.log("You are sign in " + jsonData.username);
-	    } else {
-		
-		res.sendfile('/public/index.html', {root:__dirname});
+	connection.query('select * from hdmDb where id = ' + 1, function(err,result){
+    if (req.body == result){
+		console.log("You are sign in " );
+	}else{
 		console.log("Incorrect email or password. Try again!");
-	    }
-//	}
-	})
+	}
+	res.end('----res------');
+	});
 };
 
 module.exports.put = function(req, res){
@@ -56,4 +50,4 @@ module.exports.put = function(req, res){
 
 module.exports.delete = function(req, res) {
   	res.end('---delete---');
-  }
+}
---
+++ Edgar_Hakobyan/Homework/node_js/signin_signup/modules/user/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE hdmDb (
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    username TEXT NOT NULL,
+    email    TEXT NOT NULL,
+    password TEXT NOT NULL
+);

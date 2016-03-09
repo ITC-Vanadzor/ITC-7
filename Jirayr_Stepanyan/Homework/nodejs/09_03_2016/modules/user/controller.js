658aeb7490e4e3a9f18e5a8717aaf358884d961e
jiro <jiro-19944@mail.ru>
2016-03-10 00:37:06 +0400

    added hdm's signUp with joi incomplete version

--- Jirayr_Stepanyan/Homework/nodejs/09_03_2016/modules/user/controller.js
+++ Jirayr_Stepanyan/Homework/nodejs/09_03_2016/modules/user/controller.js
@@ -1,5 +1,6 @@
 var db = require('./db').mySql;
 var sha256 = require('js-sha256');
+var joi = require('joi');
 module.exports.get = function(req, res)
 {
 	res.end('------get------');
@@ -7,40 +8,47 @@ module.exports.get = function(req, res)
 
 module.exports.signUp = function(req, res) {
     var data = req.body;
-    var valid = validateUserData(data);
-    if (valid) 
-    {
-        var password = transformPasword(data.password);
-        password = sha256(password);
-        var verify = 'SElECT * from hdm where email="' + data.email + '"';
-        db.query(verify, function(err, rows) 
-        {
-            if (rows[0]) 
-            {
-                res.end('You are already signed up');
-                return;
-            }
-            else
-            {
-		        var query = 'INSERT INTO hdm set email = "' + data.email + '", password = "' + password + '"';
-		        db.query(query, function(err, success) 
-		        {
-		            if (err) 
-		            {
-		                res.end('can\'t create user');
-		            } else 
-		            {
-		                res.end('user was created');
-		            }
-		        });
-    		}
-    	});	 
-    }
-    else 
-    {
-        res.end('validtion error');
-    }
-};
+    var schema = joi.object().keys({
+    	password: Joi.string().min(6),
+    	email: Joi.string().email()
+	});
+	console.log('---------------------------------------------');
+    joi.validate({ email: data.email, password: data.password }, schema, function (err, value) 
+    { 
+    	if (err)
+    	{
+    		res.end('----validation error----');
+    		return;
+    	}
+    	else
+    	{   
+	        var password = transformPasword(data.password);
+	        password = sha256(password);
+	        var verify = 'SElECT * from hdm where email="' + data.email + '"';
+	        db.query(verify, function(err, rows) 
+	        {
+	            if (rows[0]) 
+	            {
+	                res.end('You are already signed up');
+	                return;
+	            }
+	            else
+	            {
+			        var query = 'INSERT INTO hdm set email = "' + data.email + '", password = "' + password + '"';
+			        db.query(query, function(err, success) 
+			        {
+			            if (err) 
+			            {
+			                res.end('can\'t create user');
+			            } else 
+			            {
+			                res.end('user was created');
+			            }
+			        });
+	    		}
+	    	});	 
+    	}
+	};
 
 module.exports.signIn = function(req, res) {
     var data = req.body;
@@ -67,14 +75,7 @@ module.exports.signIn = function(req, res) {
     }
 };
 
-var validateUserData = function(data) {
-    var valid = false;
-    if (data.email && data.password) {
-        valid = validateEmail(data.email);
-        valid = valid && data.password.length >= 6;
-    }
-    return valid;
-};
+
 
 function transformPasword(password) {
     var index = Math.round(password.length / 2);
@@ -82,13 +83,6 @@ function transformPasword(password) {
     password = salt + password + salt;
     return password;
 }
-
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
-
 
 
 module.exports.put = function(req, res)

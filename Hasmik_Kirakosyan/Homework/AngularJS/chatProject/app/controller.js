6b04006d9bea5e8bbbdbe4b6cb4f1e8fdd02e551
hasmikus@gmail.com <Hasmik>
2016-03-16 00:14:46 +0400

    Added profile page to chatProject

--- Hasmik_Kirakosyan/Homework/AngularJS/chatProject/app/controller.js
+++ Hasmik_Kirakosyan/Homework/AngularJS/chatProject/app/controller.js
@@ -1,7 +1,13 @@
 var app=angular.module("myApp", []);
-app.controller('myCtrl',function ($scope) {
-	$scope.changeDivId = function(param) {
-		$scope.divId = param;
-	}	
 
+app.controller('myCtrl',function ($scope) {
+		  $scope.changeDivId = function(param) {
+					 $scope.divId = param;
+		  }	
+
+		  $scope.objArray = [];
+		  $scope.addUser = function() {
+					 $scope.obj = {name:$scope.name, email:$scope.email}
+				 $scope.objArray = $scope.objArray.concat([$scope.obj]);
+		  }
 });

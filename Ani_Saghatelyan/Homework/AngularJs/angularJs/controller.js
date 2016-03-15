e665aa7cfe8568d226bf16ae89bbabaf2c76ec5b
Ani121293 <ani_saghatelyan@mail.ru>
2016-03-16 03:22:06 +0400

    Added the usage.html in angular-Js

--- Ani_Saghatelyan/Homework/AngularJs/angularJs/controller.js
+++ Ani_Saghatelyan/Homework/AngularJs/angularJs/controller.js
@@ -1,29 +1,36 @@
 	var app=angular.module("app",[]);
-	app.controller.usageController= function($scope){
-	$scope.progressSteps=function(step, Circle, idStep) {
-		$scope.One=false;
-		$scope.Two= false;
-		$scope.Three=false;
-		$scope.step=true;
-	
-		$scope.thirdCircle={"background": "linear-gradient(to top, #0094F7, #007acc)"}
-		$scope.secondCircle={"background": "linear-gradient(to top, #0094F7, #007acc)"}
-		$scope.firstCircle={"background": "linear-gradient(to top, #0094F7, #007acc)"}
-        
-	        $scope.Circle={"background":  "#999999"}
-    	
-		$scope.thirdProgress={"background": "#bfbfbf"}
-    		$scope.secondProgress={"background": "#bfbfbf"}
-    		$scope.firstProgress={"background": "#bfbfbf"}
-    switch (step) {
-        case 'thirdProgress':
-               $scope.thirdProgress={"background": "#007acc"};
-        case 'secondProgress':
-            $scope.secondProgress={"background":"#007acc"};
-        case 'firstProgress':
-            $scope.firstProgress = {"background": "#007acc"};
-    }
+	app.controller('usageController', function($scope) {
 
-}
-
-}
+	$scope.showDiv=function(num){
+			$scope.div1=false;
+ 			$scope.div2=false;
+			$scope.div3=false;
+			$scope.buttonStep1={"background":"linear-gradient(to top, #0094F7, #007acc)"};
+			$scope.buttonStep2={"background":"linear-gradient(to top, #0094F7, #007acc)"};
+			$scope.buttonStep3={"background":"linear-gradient(to top, #0094F7, #007acc)"};
+			$scope.progress1={"background":"#bfbfbf"};
+			$scope.progress2={"background":"#bfbfbf"};
+			$scope.progress3={"background":"#bfbfbf"};
+	switch(num){
+		case 1: 
+			$scope.div1=true;
+			$scope.buttonStep1={"background":"#999999"};
+			break;
+		case 2:
+      		        $scope.div2=true;
+			$scope.buttonStep2={"background":"#999999"};
+			break;
+		case 3: 
+			$scope.div3=true;
+			$scope.buttonStep3={"background":"#999999"};
+			break;
+	}
+	switch(num){
+		case 3: $scope.progress3={"background":"#007acc"};
+		case 2: $scope.progress2={"background":"#007acc"};
+		case 1: $scope.progress1={"background":"#007acc"};
+	}
+		
+  }
+	$scope.showDiv(1);
+});

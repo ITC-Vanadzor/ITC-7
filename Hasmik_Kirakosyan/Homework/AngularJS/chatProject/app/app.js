414feac6b234858b07ac2cf2df40453a92630847
hasmikus@gmail.com <Hasmik>
2016-03-17 01:44:57 +0400

    Added updated chatProject (added ng-includes)

--- Hasmik_Kirakosyan/Homework/AngularJS/chatProject/app/app.js
+++ Hasmik_Kirakosyan/Homework/AngularJS/chatProject/app/app.js
@@ -16,3 +16,4 @@ app.service("users", function() {
 					 }
 		  }
 });
+
--- Hasmik_Kirakosyan/Homework/AngularJS/chatProject/app/chatController.js
+++ Hasmik_Kirakosyan/Homework/AngularJS/chatProject/app/chatController.js
@@ -1,5 +1,5 @@
+
 app.controller("chatCntrl", function($scope, users) {
 	$scope.usersList = users.usersArr;
-
-
+	console.log(users.usersArr[0]);
 });

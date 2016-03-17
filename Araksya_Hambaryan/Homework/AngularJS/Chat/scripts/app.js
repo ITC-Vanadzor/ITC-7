4aabc3190b2193184b08585fc37f80f7f8fb38f8
Araks <araksyahambaryan@mail.ru>
2016-03-18 01:41:03 +0400

    updated chat.html

--- Araksya_Hambaryan/Homework/AngularJS/Chat/scripts/app.js
+++ Araksya_Hambaryan/Homework/AngularJS/Chat/scripts/app.js
@@ -1,34 +1,41 @@
 var myApp = angular.module('myApp',['ngRoute']);
 
 myApp.service('data', function() {
-    this.users = [];
+    this.users = [{
+	firstName : "Ani",
+	lastName:"Hambaryan",
+	name: "Ani Hambaryan",
+	email: "ani@gmail.com",
+	password : "123456"
+	}, {	
+	firstName : "Anna",
+	lastName:"Avetyan",
+	name: "Anna Avetyan",
+	email: "anna@gmail.com",
+	password : "456789"
+	}];	
+	
     this.chats = [];
-    this.pages = {
-	reg : {	ctrl: "formCtrl", url : "src/registracion.html"},
-	chat : {ctrl: "chatCtrl", url : "src/chats.html"},
-	profile : {ctrl: "profileCtrl",	url: "src/profile.html"}
-	};
     
 });
 
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/', {
-            templateUrl: '../src/registration.html',
-            controller: 'formCtrl'
-        }).
+	    templateUrl: 'registration.html',
+	    controller: 'formCtrl'
+	}).
 
-        when('/chats', {
-            templateUrl: '../src/chats.html',
+	when('/chats', {
+	    templateUrl: 'chats.html',
             controller: 'chatCtrl'
-        }).
+	}).
 
-        when('/profile', {
-            templateUrl: '../src/profile.html',
+	when('/profile', {
+	    templateUrl: 'profile.html',
             controller: 'profileCtrl'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
+	}).
+	otherwise({
+	    redirectTo: '/'
+	});
 }]);
-
--- Araksya_Hambaryan/Homework/AngularJS/Chat/scripts/chatController.js
+++ Araksya_Hambaryan/Homework/AngularJS/Chat/scripts/chatController.js
@@ -1,9 +1,10 @@
 myApp.controller('chatCtrl', function($scope, data) {
+    $scope.allUsers= data.users;
     $scope.chat = {};
     $scope.sendMessage =function() {
         data.chats.push($scope.chat);
         $scope.chat = {};
         //alert("You already have an account");
-	$scope.page = "chats.html";
+	console.log(data.chats);
     };
 });

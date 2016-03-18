48ab57f9080179f64679d17e82bcfe74d79791cc
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-03-18 12:43:48 +0400

    added profile.html

--- Edgar_Hakobyan/Homework/angularjs/chat/js/controllers/profileController.js
+++ Edgar_Hakobyan/Homework/angularjs/chat/js/controllers/profileController.js
@@ -1,3 +1,11 @@
 myApp.controller('profileController',function($scope,dataService){
-    
+    $scope.profile = function(){
+        for(i in dataService.users){
+            if($scope.name == dataService.users[i].name && $scope.surname == dataService.users[i].surname){
+                console.log(dataService.users[i].name);
+                console.log(dataService.users[i].surname);
+                console.log(dataService.users[i].email);
+            }
+        }
+    }
 });

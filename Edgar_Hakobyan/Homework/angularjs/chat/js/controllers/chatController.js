7598d0f51d2d00d53bd5bc877a3f9e1a40088553
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-03-20 23:31:30 +0400

    chat-i verjnakan tesq@

--- Edgar_Hakobyan/Homework/angularjs/chat/js/controllers/chatController.js
+++ Edgar_Hakobyan/Homework/angularjs/chat/js/controllers/chatController.js
@@ -1,28 +1,17 @@
 myApp.controller('chatController',function($scope,dataService){
-    $scope.newMessage = function(){
-        console.log(dataService.users);
-        var sUser = 0;
-        var rUser = 0;
-        for (i in dataService.users){
-            console.log(dataService.users[i].name);
-            if($scope.senderName == dataService.users[i].name && $scope.senderSurname == dataService.users[i].surname){
-                sUser = 1;
-                for(j in dataService.users){
-                    if($scope.recipientName == dataService.users[j].name && $scope.recipientSurname == dataService.users[j].surname){
-                        rUser = 1;
-                        break;
-                    }
-                }
+    $scope.model=[];
+    for (k in dataService.users){
+        $scope.model.push(dataService.users[k].name+' '+dataService.users[k].surname);
+    }
+    $scope.sendMessage = function(){
+        var recmodel='';
+        for (k in dataService.users){
+            recmodel = dataService.users[k].name+' '+dataService.users[k].surname;
+            if($scope.recipient == recmodel){
+                dataService.users[k].message.push({sender:$scope.sender,message:$scope.message});
+                console.log(dataService.users);
+                break;
             }
         }
-        if(sUser == 1 && rUser == 1){
-            dataService.users[j].message.push({sendersName:$scope.senderName,sendersSurname:$scope.senderSurname,message:$scope.message});
-            alert("Your message is sent");
-        }else if (sUser == 0){
-            alert("there is no such user");
-        }else{
-            alert("you are not registered");
-        }
-    };
-    console.log(dataService.users);
+    }
 });

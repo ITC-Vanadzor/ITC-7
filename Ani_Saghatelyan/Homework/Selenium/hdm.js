6dbf6c6872596b50fd8e3a307a611498dcf3af4f
Ani121293 <ani_saghatelyan@mail.ru>
2016-03-22 01:21:34 +0400

    Added selenium test for sign-in sign-up

---
+++ Ani_Saghatelyan/Homework/Selenium/hdm.js
@@ -0,0 +1,28 @@
+var webdriver = require('selenium-webdriver'),
+	By = webdriver.By,
+   	 until = webdriver.until;
+var randomstring = require("randomstring");
+	
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://localhost:8080');
+driver.findElement(By.linkText("Sign Up")).click();
+
+driver.findElement(By.id("addName")).sendKeys("chebureki");
+var myEmail=randomstring.generate(7)+'@mail.ru'
+driver.findElement(By.id("addEmail")).sendKeys(myEmail);
+driver.findElement(By.id("addPassword")).sendKeys("chebureki");
+driver.findElement(By.id("registrationButton")).click();
+
+
+driver.navigate().back();
+driver.navigate().back();
+
+driver.findElement(By.className("grayButton")).click();
+driver.findElement(By.id("iEmail")).sendKeys(myEmail);
+driver.findElement(By.id("iPassword")).sendKeys("chebureki");
+driver.findElement(By.id("signInId")).click();
+driver.sleep(10000);
+driver.quit();

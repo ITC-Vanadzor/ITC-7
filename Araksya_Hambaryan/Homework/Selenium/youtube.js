79533f64a9bce03df1807d17952e5c53d333bc33
Araks <araksyahambaryan@mail.ru>
2016-03-22 22:54:41 +0400

    Added HDM test with chrome and firefox, and youtube incomplete cersion

--- Araksya_Hambaryan/Homework/Selenium/youtube.js
+++ Araksya_Hambaryan/Homework/Selenium/youtube.js
@@ -9,14 +9,34 @@ var driver = new webdriver.Builder()
 
 driver.get("http://www.youtube.com");
 driver.findElement(By.className("yt-uix-button yt-uix-button-size-default yt-uix-button-primary")).click();
+driver.sleep(6000);
 driver.findElement(By.id("Email")).sendKeys("itc7vanadzor@gmail.com");
 driver.findElement(By.id("next")).click();
+driver.sleep(6000);
 driver.findElement(By.id("Passwd")).sendKeys("instigate");
 driver.findElement(By.id("PersistentCookie")).click();
 driver.findElement(By.id("signIn")).click();
+driver.sleep(6000);
 driver.findElement(By.id("masthead-search-term")).sendKeys("Beth Hart");
 driver.findElement(By.id("search-btn")).click();
 driver.executeScript("window.scroll(0,document.body.scrollHeight);");
-driver.findElement(By.id("item-section-724536").lastChild).click();
-driver.sleep(10000);
+driver.sleep(8000);
+	
+driver.findElement(By.css('#results .item-section li:last-child .yt-lockup')).click();
+driver.sleep(6000);
+driver.findElement(By.css('#watch8-secondary-actions > button')).click();
+//driver.findElement(By.css('[type=checkbox]')).click();
+var url= driver.findElement(By.className("yt-uix-form-input-text share-panel-url")).getAttribute("value");
+driver.sleep(15000);
+
+driver.navigate().to("https://gmail.com");
+driver.findElement(By.className("T-I J-J5-Ji T-I-KE L3")).click();
+driver.sleep(6000);
+driver.findElement(By.id(":9y")).sendKeys('itc7vanadzor@gmail.com');
+driver.findElement(By.id(":9i")).sendKeys('Smbat');
+driver.findElement(By.id(":an")).sendKeys(url);
+driver.findElement(By.id(":98")).click();
+
+driver.wait(100000);
+
 driver.quit();

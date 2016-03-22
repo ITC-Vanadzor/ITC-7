33d71b8c2898c357366052fa94c35cf2ccd0614a
Hakob Torosyan <htorosyan.1990@gmail.com>
2016-03-23 01:02:57 +0400

    Add solutions for hdm for selenium and incomplate version for youtube

--- Hakob_Torosyan/Selenium/sel.js
+++ Hakob_Torosyan/Selenium/sel.js
@@ -7,16 +7,26 @@ var driver = new webdriver.Builder()
     .build();
 
 driver.get('https://youtube.com/');
+driver.findElement(By.className("yt-uix-button yt-uix-button-size-default yt-uix-button-primary")).click();
+driver.findElement(By.id("account-chooser-add-account")).click();
+driver.sleep(3000);
+driver.findElement(By.id("Email")).sendKeys("sparapethayoc451@gmail.com");
+driver.sleep(3000);
+driver.findElement(By.id("next")).click();
+driver.sleep(3000);
+driver.findElement(By.id("Passwd")).sendKeys("avarayr451");
+driver.sleep(3000);
+driver.findElement(By.id("signIn")).click();
+driver.sleep(3000);
+driver.get('https://youtube.com/');
+driver.sleep(3000);
 driver.executeScript("window.scrollTo(0, document.body.scrollHeight)", "");
+driver.sleep(3000);
 driver.findElement(By.id("masthead-search-term")).sendKeys("Real Madrid Barcelona 4:1 ");
+driver.sleep(3000);
 driver.findElement(By.id("search-btn")).click();
-driver.findElement(By.id("yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 spf-link")).click();
+//driver.findElement(By.className("yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 spf-link ")).click();
 
-//driver.findElement(By.linkText("Sign in")).
 
-//driver.findElement(By.id("loginbutton")).click();
-//driver.sleep(10000);
-//driver.findElement(By.className("project")).click();
-//driver.findElement(By.className("fa fa-download")).click();
 driver.sleep(10000);
 driver.quit();

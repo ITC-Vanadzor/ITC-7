395b97619824a458fb80b1ba585f362a14f0f162
Ani121293 <ani_saghatelyan@mail.ru>
2016-03-22 17:02:44 +0400

    Added hdm project corrected version

--- Hasmik_Kirakosyan/Homework/Selenium/hdm/testSignUpSignIn.js
+++ Hasmik_Kirakosyan/Homework/Selenium/hdm/testSignUpSignIn.js
@@ -1,22 +1,36 @@
 var webdriver = require('selenium-webdriver'),
-		  			 By = webdriver.By,
-			   	 until = webdriver.until;
-	
-var driver = new webdriver.Builder()
-		      .forBrowser('firefox')
-				    .build();
+    By = webdriver.By,
+    until = webdriver.until;
 
-					 driver.get('http://127.0.0.1:8080/');
-					 driver.findElement(By.linkText("Sign Up")).click();
-					 driver.findElement(By.id("addName")).sendKeys("testName");
-					 var email = "testEmail"+Math.floor(Math.random() * 57)+"@gmail.com";
-					 driver.findElement(By.id("addEmail")).sendKeys(email);
-					 driver.findElement(By.id("addPassword")).sendKeys("secretPassword22!");
-					 driver.findElement(By.name("signUp")).click();
-					 //driver.sleep(10000);
-					 driver.get('http://127.0.0.1:8080/src/signin.html');
-					 driver.findElement(By.id("iEmail")).sendKeys(email);
-					 driver.findElement(By.id("iPassword")).sendKeys("secretPassword22!");
-					 driver.findElement(By.linkText("Sign In")).click();
-					 driver.sleep(10000);
-					 driver.quit();
+var driver = new webdriver.Builder()
+	.forBrowser('firefox')
+	.build();
+
+driver.get('http://127.0.0.1:8080/');
+driver.findElement(By.linkText("Sign Up")).click();
+driver.findElement(By.id("addName")).sendKeys("testName");
+var email = "testEmail"+Math.floor(Math.random() * 57)+"@gmail.com";
+driver.findElement(By.id("addEmail")).sendKeys(email);
+var password = "secretsecret22"
+driver.findElement(By.id("addPassword")).sendKeys(password);
+
+driver.findElement(By.css("#signUpDiv .loginBg button")).click();
+
+driver.navigate().back();
+driver.navigate().back();
+
+driver.findElement(By.linkText("Sign In")).click();
+
+driver.findElement(By.id("iEmail")).sendKeys(email);
+
+driver.findElement(By.id("iPassword")).sendKeys(password);
+
+driver.findElement(By.css("#signInDiv form button")).click();
+
+ driver.wait(function() {
+   return driver.getTitle().then(function(title) {
+     return title === 'HDM Statistic';
+   });
+ }, 1000);
+driver.sleep(4000);
+driver.quit();

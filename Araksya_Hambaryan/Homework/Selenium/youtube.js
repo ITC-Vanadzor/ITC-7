53ce274f9e5afbf29c53cb056a26795e4f32c164
Araks <araksyahambaryan@mail.ru>
2016-03-22 13:17:48 +0400

    updeted selenium task for youtube

--- Araksya_Hambaryan/Homework/Selenium/youtube.js
+++ Araksya_Hambaryan/Homework/Selenium/youtube.js
@@ -1,20 +1,22 @@
+require("chromedriver");
 var webdriver = require('selenium-webdriver'),
-	By = webdriver.By,
-   	 until = webdriver.until;
-	
-var driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .build();
+    By = webdriver.By,
+    until = webdriver.until;
 
-driver.get('https://youtube.com/');
+var driver = new webdriver.Builder()
+   .forBrowser("firefox")
+   .build();
+
+driver.get("http://www.youtube.com");
 driver.findElement(By.className("yt-uix-button yt-uix-button-size-default yt-uix-button-primary")).click();
 driver.findElement(By.id("Email")).sendKeys("itc7vanadzor@gmail.com");
-driver.findElement(By.name("next")).click();
+driver.findElement(By.id("next")).click();
 driver.findElement(By.id("Passwd")).sendKeys("instigate");
-driver.findElement(By.name("PersistentCookie")).click();
-driver.findElement(By.name("signIn")).click();
+driver.findElement(By.id("PersistentCookie")).click();
+driver.findElement(By.id("signIn")).click();
 driver.findElement(By.id("masthead-search-term")).sendKeys("Beth Hart");
 driver.findElement(By.id("search-btn")).click();
-//((JavascriptExecutor) driver).executeScript("scroll(0,250);");
+driver.executeScript("window.scroll(0,document.body.scrollHeight);");
+driver.findElement(By.id("item-section-724536").lastChild).click();
 driver.sleep(10000);
 driver.quit();

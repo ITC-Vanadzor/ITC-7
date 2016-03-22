1a2cedad089fdd4421b6a2bc08936bca41b1494f
Araks <araksyahambaryan@mail.ru>
2016-03-23 00:52:09 +0400

    updated

--- Araksya_Hambaryan/Homework/Selenium/youtube.js
+++ Araksya_Hambaryan/Homework/Selenium/youtube.js
@@ -10,7 +10,7 @@ var driver = new webdriver.Builder()
 driver.get("http://www.youtube.com");
 driver.findElement(By.className("yt-uix-button yt-uix-button-size-default yt-uix-button-primary")).click();
 driver.sleep(6000);
-driver.findElement(By.id("Email")).sendKeys("itc7vanadzor@gmail.com");
+driver.wait(until.elementLocated(By.id("Email")), 5*1000).then(function(elm){elm.sendKeys("itc7vanadzor@gmail.com")});
 driver.findElement(By.id("next")).click();
 driver.sleep(6000);
 driver.findElement(By.id("Passwd")).sendKeys("instigate");
@@ -22,17 +22,17 @@ driver.findElement(By.id("search-btn")).click();
 driver.executeScript("window.scroll(0,document.body.scrollHeight);");
 driver.sleep(8000);
 	
-driver.findElement(By.css('#results .item-section li:last-child .yt-lockup')).click();
+driver.wait(until.elementLocated(By.css('#results .item-section li:last-child .yt-lockup')), 5*1000).then(function(elm){elm.click()});
 driver.sleep(6000);
-driver.findElement(By.css('#watch8-secondary-actions > button')).click();
+driver.findElement(By.css('#watch8-secondary-actions > .action-panel-trigger-share')).click();
 //driver.findElement(By.css('[type=checkbox]')).click();
 var url= driver.findElement(By.className("yt-uix-form-input-text share-panel-url")).getAttribute("value");
 driver.sleep(15000);
 
 driver.navigate().to("https://gmail.com");
-driver.findElement(By.className("T-I J-J5-Ji T-I-KE L3")).click();
+driver.wait(until.elementLocated(By.className("T-I J-J5-Ji T-I-KE L3")), 5*1000).then(function(elm){elm.click()});
 driver.sleep(6000);
-driver.findElement(By.id(":9y")).sendKeys('itc7vanadzor@gmail.com');
+driver.wait(until.elementLocated(By.id(":9y")), 5*1000).then(function(elm){elm.sendKeys('itc7vanadzor@gmail.com')});
 driver.findElement(By.id(":9i")).sendKeys('Smbat');
 driver.findElement(By.id(":an")).sendKeys(url);
 driver.findElement(By.id(":98")).click();

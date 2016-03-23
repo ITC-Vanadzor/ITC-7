9f0e4b3fbb089dec0efaf50ea9c4a08323478dde
GevorVardanyan <gevor.vardanyan.93@mail.ru>
2016-03-23 11:42:17 +0400

    hdmTest

---
+++ Gevor_Vardanyan/Homeworks/Selenium/hdmTest.js
@@ -0,0 +1,32 @@
+var randomstring = require("randomstring");
+var webDr = require('selenium-webdriver'),
+By = webDr.By,
+until = webDr.until;
+
+var driver = new webDr.Builder()
+    .forBrowser('firefox')
+    .build();
+
+	var name=randomstring.generate(7);
+	var email=randomstring.generate(7)+'@'+randomstring.generate(4)+'.com';
+	var pass=randomstring.generate(6);
+
+	driver.get('http://localhost:8080');
+	driver.findElement(By.className('blueButton')).click();
+	
+	driver.findElement(By.id('addName')).sendKeys(name);
+	driver.findElement(By.id('addEmail')).sendKeys(email);
+	driver.findElement(By.id('addPassword')).sendKeys(pass);
+	
+
+
+	driver.findElement(By.css('#signUpDiv button')).click();
+	driver.get('http://localhost:8080/src/signin.html');
+
+	driver.findElement(By.id('iEmail')).sendKeys(email);
+	driver.findElement(By.id('iPassword')).sendKeys(pass);
+
+	driver.findElement(By.css('#signInDiv button')).click();
+	driver.wait(until.titleIs('HDM Statistic'),5000);
+ 
+driver.quit();

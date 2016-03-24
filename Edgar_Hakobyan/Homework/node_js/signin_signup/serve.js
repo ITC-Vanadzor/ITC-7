77246b7d7ec5be4b27dd480a1163df372c55e2cf
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-03-25 03:01:28 +0400

    Update serve.js

--- Edgar_Hakobyan/Homework/node_js/signin_signup/serve.js
+++ Edgar_Hakobyan/Homework/node_js/signin_signup/serve.js
@@ -7,7 +7,7 @@ app = require('./config')(app);
 require('./router/')(app);
 
 
-server.listen(8080, 'localhost', function () {
+server.listen(8090, 'localhost', function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);

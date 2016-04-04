a4cc8798f4545fa718e3f7199cede52f643d9627
Smbat888 <Sargsyanyan@mail.ru>
2016-04-04 22:49:20 +0400

    added vanadzor_guide db structure code with mysql

---
+++ Smbat_Sargsyan/homework/DataBase/db_file.sql
@@ -0,0 +1,261 @@
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: vanadzor_guide
+-- ------------------------------------------------------
+-- Server version	5.5.47-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `card`
+--
+
+DROP TABLE IF EXISTS `card`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `card` (
+  `id_card` int(11) NOT NULL AUTO_INCREMENT,
+  `icon` int(11) DEFAULT NULL,
+  `title` varchar(15) NOT NULL,
+  `id_type` int(11) NOT NULL,
+  `image` int(11) NOT NULL,
+  `city_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_card`),
+  KEY `img_fk` (`image`),
+  KEY `type_fk` (`id_type`),
+  KEY `city_id_fk` (`city_id`),
+  CONSTRAINT `city_id_fk` FOREIGN KEY (`city_id`) REFERENCES `city` (`c_id`),
+  CONSTRAINT `img_fk` FOREIGN KEY (`image`) REFERENCES `images` (`id_card`),
+  CONSTRAINT `type_fk` FOREIGN KEY (`id_type`) REFERENCES `sub_category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card`
+--
+
+LOCK TABLES `card` WRITE;
+/*!40000 ALTER TABLE `card` DISABLE KEYS */;
+/*!40000 ALTER TABLE `card` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `category` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `city` (
+  `c_id` int(11) NOT NULL,
+  `country` varchar(20) DEFAULT NULL,
+  `region` varchar(20) DEFAULT NULL,
+  `city` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`c_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `city`
+--
+
+LOCK TABLES `city` WRITE;
+/*!40000 ALTER TABLE `city` DISABLE KEYS */;
+/*!40000 ALTER TABLE `city` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detail`
+--
+
+DROP TABLE IF EXISTS `detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detail` (
+  `id_card` int(11) NOT NULL,
+  `description` blob,
+  KEY `card_id_fk` (`id_card`),
+  CONSTRAINT `card_id_fk` FOREIGN KEY (`id_card`) REFERENCES `card` (`id_card`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail`
+--
+
+LOCK TABLES `detail` WRITE;
+/*!40000 ALTER TABLE `detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id_card` int(11) NOT NULL,
+  `id` int(11) DEFAULT NULL,
+  `img` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_card`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `optional_field`
+--
+
+DROP TABLE IF EXISTS `optional_field`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `optional_field` (
+  `id` int(11) NOT NULL,
+  `field_name` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `optional_field`
+--
+
+LOCK TABLES `optional_field` WRITE;
+/*!40000 ALTER TABLE `optional_field` DISABLE KEYS */;
+/*!40000 ALTER TABLE `optional_field` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `optional_info`
+--
+
+DROP TABLE IF EXISTS `optional_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `optional_info` (
+  `id` int(11) NOT NULL,
+  `id_card` int(11) NOT NULL,
+  `field_id` int(11) NOT NULL,
+  `value` varchar(15) DEFAULT NULL,
+  KEY `opt_inf_fk` (`field_id`),
+  KEY `c_id_fk` (`id_card`),
+  CONSTRAINT `c_id_fk` FOREIGN KEY (`id_card`) REFERENCES `detail` (`id_card`),
+  CONSTRAINT `opt_inf_fk` FOREIGN KEY (`field_id`) REFERENCES `optional_field` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `optional_info`
+--
+
+LOCK TABLES `optional_info` WRITE;
+/*!40000 ALTER TABLE `optional_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `optional_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `popular`
+--
+
+DROP TABLE IF EXISTS `popular`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `popular` (
+  `id` int(11) NOT NULL,
+  `id_card` int(11) NOT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `popular_id_fk` (`id_card`),
+  CONSTRAINT `popular_id_fk` FOREIGN KEY (`id_card`) REFERENCES `card` (`id_card`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `popular`
+--
+
+LOCK TABLES `popular` WRITE;
+/*!40000 ALTER TABLE `popular` DISABLE KEYS */;
+/*!40000 ALTER TABLE `popular` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sub_category`
+--
+
+DROP TABLE IF EXISTS `sub_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sub_category` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(15) DEFAULT NULL,
+  `img` int(11) DEFAULT NULL,
+  `is_event` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cat_fk` (`category_id`),
+  CONSTRAINT `cat_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sub_category`
+--
+
+LOCK TABLES `sub_category` WRITE;
+/*!40000 ALTER TABLE `sub_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sub_category` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-04 22:46:04

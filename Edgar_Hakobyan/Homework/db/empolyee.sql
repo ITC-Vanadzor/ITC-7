b2264a839dea618d4e8ef0001224bf3b1be2d4eb
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-04-07 02:05:31 +0400

    update empolyee db

--- Edgar_Hakobyan/Homework/db/empolyee.sql
+++ Edgar_Hakobyan/Homework/db/empolyee.sql
@@ -1,4 +1,122 @@
-Usage: mysqldump [OPTIONS] database [tables]
-OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
-OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
-For more options, use mysqldump --help
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: empolyee
+-- ------------------------------------------------------
+-- Server version	5.5.47-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `empolyee_salary`
+--
+
+DROP TABLE IF EXISTS `empolyee_salary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empolyee_salary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  `position_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `empolyee_position_fk` (`position_id`),
+  CONSTRAINT `empolyee_position_fk` FOREIGN KEY (`position_id`) REFERENCES `position_level` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empolyee_salary`
+--
+
+LOCK TABLES `empolyee_salary` WRITE;
+/*!40000 ALTER TABLE `empolyee_salary` DISABLE KEYS */;
+INSERT INTO `empolyee_salary` VALUES (1,'roman',80000,1),(2,'hamo',330000,3),(3,'hamo',350000,2);
+/*!40000 ALTER TABLE `empolyee_salary` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 trigger update_position after update on empolyee_salary for each row call position_level(330000) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `position_level`
+--
+
+DROP TABLE IF EXISTS `position_level`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `position_level` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `level` int(11) DEFAULT NULL,
+  `salary_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `level_salary_fk` (`salary_id`),
+  CONSTRAINT `level_salary_fk` FOREIGN KEY (`salary_id`) REFERENCES `salary_range` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `position_level`
+--
+
+LOCK TABLES `position_level` WRITE;
+/*!40000 ALTER TABLE `position_level` DISABLE KEYS */;
+INSERT INTO `position_level` VALUES (1,1,1),(2,2,2),(3,3,3);
+/*!40000 ALTER TABLE `position_level` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salary_range`
+--
+
+DROP TABLE IF EXISTS `salary_range`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `salary_range` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `min_salary` int(11) NOT NULL,
+  `max_salary` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salary_range`
+--
+
+LOCK TABLES `salary_range` WRITE;
+/*!40000 ALTER TABLE `salary_range` DISABLE KEYS */;
+INSERT INTO `salary_range` VALUES (1,50000,200000),(2,200000,300000),(3,300000,400000);
+/*!40000 ALTER TABLE `salary_range` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-07  2:03:46

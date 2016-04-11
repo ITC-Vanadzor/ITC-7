ddb090dd44cf63a4927bc56e761cd0fbdbb6a9f1
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-04-12 01:46:27 +0400

    changed kaprekar.java

--- Edgar_Hakobyan/Homework/java/kaprekar.java
+++ Edgar_Hakobyan/Homework/java/kaprekar.java
@@ -2,22 +2,25 @@ import java.util.*;
 
 class kaprekar {
 	public static void main(String[] args) {
-		Scanner cin = new Scanner(System.in);
-		int number = cin.nextInt();
-		Vector<Integer> parseNumberStart = parse(number);
-//		int sizeVectorStart = parseNumber.size();
-		int result = max_min(parseNumberStart);
-		Vector<Integer> parseNumberResult = parse(result);
-		if(parseNumberStart.size() != parseNumberResult.size()) {
-			while(parseNumberStart.size() != parseNumberResult.size()){
-				parseNumberResult.add(0);
+		Scanner in = new Scanner(System.in);
+		int number = in.nextInt();
+		Vector<Integer> parseNumber = parse(number);
+		int size = parseNumber.size();
+		int result = max_min(parseNumber);
+	
+		if(size != parse(result).size()) {
+			parseNumber = parse(result);
+			while(size != parseNumber.size()){
+				parseNumber.add(0);
 			}
-			for(int i=parseNumberResult.size()-1; i >=0; --i) {
-				System.out.print(parseNumberResult.get(i));
+			for(int i=parseNumber.size()-1; i >=0; --i) {
+				System.out.print(parseNumber.get(i));
 			}
+			System.out.println();
 		} else {
 			System.out.println(result);
 		}
+
 	}
 	public static int max_min(Vector<Integer> vec) {
 		for(int i=vec.size()-1; i>0; --i) {
@@ -39,6 +42,7 @@ class kaprekar {
 		}
 		return maxResult-minResult;
 	}
+
 	public static Vector<Integer> parse(int num) {
 		Vector<Integer> vectorNumber = new Vector<Integer>();
 		while(num > 0) {
@@ -47,4 +51,5 @@ class kaprekar {
         }
 		return vectorNumber;
 	}
+
 }

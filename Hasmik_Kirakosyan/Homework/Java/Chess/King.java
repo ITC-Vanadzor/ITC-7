aab8c7d67e730662af138d98e88eecc25643a800
hasmikus@gmail.com <Hasmik>
2016-04-12 23:55:04 +0400

    Deleted code repetition by adding new class SetCoordinates and extends from it coins classes

--- Hasmik_Kirakosyan/Homework/Java/Chess/King.java
+++ Hasmik_Kirakosyan/Homework/Java/Chess/King.java
@@ -1,12 +1,8 @@
 /* File name : King.java */
 
-public class King implements Piece {
+public class King extends SetCoordinates implements Piece {
  	public boolean step (String pos1, String pos2) {
-		int i1 = Character.getNumericValue(pos1.charAt(0)) - 64;
-		int j1 = Character.getNumericValue(pos1.charAt(1));
-		int i2 = Character.getNumericValue(pos2.charAt(0)) - 64;	
-		int j2 = Character.getNumericValue(pos2.charAt(1));	
-		
+		changeToInts(pos1, pos2);	
 		return ( (Math.abs(i1-i2) == 1 && Math.abs(j1-j2) == 0) || 
 				   (Math.abs(j1-j2) == 1 && Math.abs(i1-i2) == 0) ||
 					(Math.abs(j1-j2) == 1 && Math.abs(i1-i2) == 1) );		 
--- Hasmik_Kirakosyan/Homework/Java/Chess/Queen.java
+++ Hasmik_Kirakosyan/Homework/Java/Chess/Queen.java
@@ -2,12 +2,7 @@
 
 public class Queen extends SetCoordinates implements Piece{
 		public boolean step (String pos1, String pos2) {
-	   //changeToInts(pos1, pos2);
-		int i1 = Character.getNumericValue(pos1.charAt(0)) - 64;
-		int j1 = Character.getNumericValue(pos1.charAt(1));
-		int i2 = Character.getNumericValue(pos2.charAt(0)) - 64;	
-		int j2 = Character.getNumericValue(pos2.charAt(1));	
-		
+		changeToInts(pos1, pos2);
 		return ( ((i1-i2) != 0 && (j1-j2) == 0)   ||
 					((j1-j2) != 0 && (i1-i2) == 0) ) ||
 				  ( Math.abs(i1-i2) == Math.abs(j1-j2));		 

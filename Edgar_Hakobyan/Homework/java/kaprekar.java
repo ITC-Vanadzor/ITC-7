8b45734f4b079ffd550d63527f0e58899973874d
v-levonyan <v_levonyan@yahoo.com>
2016-04-12 17:00:49 +0400

    edited kaprekar.java

--- Edgar_Hakobyan/Homework/java/kaprekar.java
+++ Edgar_Hakobyan/Homework/java/kaprekar.java
@@ -4,52 +4,58 @@ class kaprekar {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int number = in.nextInt();
-		Vector<Integer> parseNumber = parse(number);
+		Number n = new Number();
+		Vector<Integer> parseNumber = n.parse(number);
 		int size = parseNumber.size();
-		int result = max_min(parseNumber);
-	
-		if(size != parse(result).size()) {
-			parseNumber = parse(result);
-			while(size != parseNumber.size()){
-				parseNumber.add(0);
-			}
-			for(int i=parseNumber.size()-1; i >=0; --i) {
-				System.out.print(parseNumber.get(i));
-			}
-			System.out.println();
-		} else {
-			System.out.println(result);
-		}
+		int result = n.max_min(parseNumber);
+		n.validation(parseNumber, size, result);
 
 	}
-	public static int max_min(Vector<Integer> vec) {
+	public static class Number { 
+	    public static int max_min(Vector<Integer> vec) {
 		for(int i=vec.size()-1; i>0; --i) {
-			for(int j=0; j<i; ++j) {
-				if(vec.get(j) < vec.get(j+1)) {
-					Integer tmp = vec.get(j);
-					vec.set(j,vec.get(j+1));
-					vec.set(j+1,tmp);
-				}
+		    for(int j=0; j<i; ++j) {
+			if(vec.get(j) < vec.get(j+1)) {
+			    Integer tmp = vec.get(j);
+			    vec.set(j,vec.get(j+1));
+			    vec.set(j+1,tmp);
 			}
+		    }
 		}
 		int maxResult=0;
 		int minResult=0;
 		for(int a=0; a<vec.size(); ++a){
-			maxResult += vec.get(a) * (int)Math.pow(10,vec.size()-a-1);
+		    maxResult += vec.get(a) * (int)Math.pow(10,vec.size()-a-1);
 		}
 		for(int b=vec.size()-1; b>=0; --b){
-			minResult += vec.get(b) * (int)Math.pow(10,b);
+		    minResult += vec.get(b) * (int)Math.pow(10,b);
 		}
 		return maxResult-minResult;
-	}
+	    }
 
-	public static Vector<Integer> parse(int num) {
+	    public static Vector<Integer> parse(int num) {
 		Vector<Integer> vectorNumber = new Vector<Integer>();
 		while(num > 0) {
-            vectorNumber.add(num % 10);
-            num /= 10;
-        }
+		    vectorNumber.add(num % 10);
+		    num /= 10;
+		}
 		return vectorNumber;
+	    }
+	    public static void validation(Vector<Integer> parseNumber, int size, int result){
+		if(size != parse(result).size()) {
+		    parseNumber = parse(result);
+		    while(size != parseNumber.size()){
+			parseNumber.add(0);
+		    }
+		    for(int i=parseNumber.size()-1; i >=0; --i) {
+			System.out.print(parseNumber.get(i));
+		    }
+		    System.out.println();
+		} else {
+		    System.out.println(result);
+		}
+	    }
+	
 	}
 
 }

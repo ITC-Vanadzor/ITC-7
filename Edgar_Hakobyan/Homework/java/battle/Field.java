ecf1e15b8707346435aec0e9f14b8d8d565b4957
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-04-15 01:04:55 +0400

    Went through homework and added the corresponding notes.

--- Edgar_Hakobyan/Homework/java/battle/Field.java
+++ Edgar_Hakobyan/Homework/java/battle/Field.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Field {
     private int n;
+// // The rules of the incapsulation are not kept
     public int[][] array;
     public Field(int size) {
         n = size;
         initialization();
     }
+// public -> private
     public void initialization() {
         Scanner in = new Scanner(System.in);
         array = new int[n][n];
@@ -23,7 +25,7 @@ public class Field {
             System.out.println();
         }
     }
-
+//public -> private
     public int getSize() {
         return n;
     }

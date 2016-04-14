8154230cffa4f36e3a534b97fae4102b3aae972c
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-04-14 21:45:22 +0400

    finalized battle and kaprekar

--- Edgar_Hakobyan/Homework/java/kaprekar/Number.java
+++ Edgar_Hakobyan/Homework/java/kaprekar/Number.java
@@ -1,37 +1,47 @@
 import java.util.*;
+
 class Number {
-		public int max_min(Vector<Integer> vec) {
-				Collections.sort(vec);
-				int maxResult=0;
-				int minResult=0;
-				for(int i=0; i<vec.size(); ++i){
-						minResult += vec.get(i) * (int)Math.pow(10,vec.size()-i-1);
-				}
-				for(int j=vec.size()-1; j>=0; --j){
-						maxResult += vec.get(j) * (int)Math.pow(10,j);
-				}
-				return maxResult-minResult;
-		}
-		public Vector<Integer> parse(int num) {
-				Vector<Integer> vectorNumber = new Vector<Integer>();
-				while(num > 0) {
-						vectorNumber.add(num % 10);
-						num /= 10;
-				}
-				return vectorNumber;
-		}
-		public void validation(Vector<Integer> parseNumber, int size, int result){
-				if(size != parse(result).size()) {
-						parseNumber = parse(result);
-						while(size != parseNumber.size()){
-								parseNumber.add(0);
-						}
-						for(int i=parseNumber.size()-1; i >=0; --i) {
-								System.out.print(parseNumber.get(i));
-						}
-						System.out.println();
-				} else {
-						System.out.println(result);
-				}
-		}
+
+    private int number; 
+    private Vector<Integer> vectorNumber;  
+    private int maxNumber=0;
+    private int minNumber=0;
+
+    public Number(int n) {
+        number = n;
+        vectorNumber = parse();
+    }
+    
+    private Vector<Integer> parse() {
+        vectorNumber = new Vector<Integer>();
+        while(number > 0) {
+            vectorNumber.add(number % 10);
+            number /= 10;
+        }
+        Collections.sort(vectorNumber);
+        return vectorNumber;
+    }
+    
+    public int max() {
+        for(int j=vectorNumber.size()-1; j>=0; --j){
+            maxNumber += vectorNumber.get(j) * (int)Math.pow(10,j);
+        }
+        return maxNumber;
+    }
+
+    public int min() {
+        for(int i=0; i<vectorNumber.size(); ++i){
+            minNumber += vectorNumber.get(i) * (int)Math.pow(10,vectorNumber.size()-i-1);
+        }
+        return minNumber;
+    }
+
+    public int diff() {
+        return maxNumber - minNumber;
+    }
+
+    public String form() {
+        String diff  = "%0" + vectorNumber.size() + "d\n";
+        return diff;
+    }
 }

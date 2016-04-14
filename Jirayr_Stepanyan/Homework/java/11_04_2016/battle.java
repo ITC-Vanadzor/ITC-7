5b80b707a14a67b47d4580d1f050ef16989e878d
Jirayr <jiro-19944@mail.ru>
2016-04-14 21:07:37 +0400

    corrected battle.java and kaprekar.java

--- Jirayr_Stepanyan/Homework/java/11_04_2016/battle.java
+++ Jirayr_Stepanyan/Homework/java/11_04_2016/battle.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 
-class battle 
+public class battle
 {
 	public static int[][] qartez = {
-		{1, 1, 1, 0},
-		{0, 0, 1, 0},
 		{0, 1, 1, 0},
-		{0, 0, 0, 0}
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
+		{0, 0, 1, 0}
 	};
 	public static int meker;
 	public static int j_max;
@@ -16,6 +16,7 @@ class battle
 
     public static void main(String[] args) 
     {
+        BattleStugum battleStugum = new BattleStugum();
     	int naver = 0;
     	for(int i = 0; i < 4; ++i)
     	{
@@ -26,7 +27,7 @@ class battle
     				j_max = j;
     				j_min = j;
     				i_max = i;
-    				stugum(qartez, i, j);
+    				battleStugum.stugum(qartez, i, j);
     				if(meker == ((j_max - j_min) + 1) * (i_max + 1))
     				{
     					++naver;
@@ -35,39 +36,5 @@ class battle
     		}
     	}
     	System.out.print("naveri qanak@ = " + naver);
-    }
-
-    public static void stugum(int qartez[][], int i, int j)
-    {
-    	++meker;
-    	qartez[i][j] = 2;
-    	if(j + 1 < 4 && qartez[i][j + 1] == 1)
-    	{
-    		if(j >= j_max)
-    		{
-    			j_max = j + 1;
-    		}
-    		stugum(qartez, i, j + 1);
-    	}
-    	if(i + 1 < 4 && qartez[i + 1][j] == 1)
-		{
-			if(i >= i_max)
-			{
-				i_max = i + 1;
-			}
-			stugum(qartez, i + 1, j);
-		}
-		if(j - 1 >= 0 && qartez[i][j - 1] == 1)
-		{
-			if(j_min <= j)
-			{
-				j_min = j - 1;
-			}
-			stugum(qartez, i, j - 1);
-		}
-		if(i - 1 >= 0 && qartez[i - 1][j] == 1)
-		{
-			stugum(qartez, i - 1, j);
-		}
     }
 }
--- Jirayr_Stepanyan/Homework/java/11_04_2016/kaprekari_max_min.java
+++ Jirayr_Stepanyan/Homework/java/11_04_2016/kaprekari_max_min.java
@@ -1,26 +1,16 @@
+import java.util.Collections
 public class Number 
     {
 		public void max_min(Vector<Integer> a)
 		{
-		    for(int i = a.size() - 1; i > 0; --i)
-		    {
-			for(int j = 0; j < i; ++j)
-			{
-			    if(a.get(j) < a.get(j+1))
-			    {
-				Integer tmp = a.get(j);
-				a.set(j, a.get(j + 1));
-				a.set(j + 1, tmp);
-			    }
-			}
-		    }
+		    Collections.sort(a);
 		    for(int t = 0; t < a.size(); ++t)
 		    {
-			max += a.get(t) * Math.pow(10, (a.size() - (t + 1)));
+				max += a.get(t) * Math.pow(10, (a.size() - (t + 1)));
 		    }
 		    for(int c = a.size() - 1; c >= 0; --c)
 		    {
-			min += a.get(c) * Math.pow(10, (c));
+				min += a.get(c) * Math.pow(10, (c));
 		    }
 		}
 
@@ -30,29 +20,29 @@ public class Number
 		    int counter = 0;
 		    while(tmp > 0)
 		    {
-			tmp /= 10;
-			++counter;
+				tmp /= 10;
+				++counter;
 		    }
 		    if(counter == n.size())
 		    {
-			System.out.println(m);
+				System.out.println(m);
 		    }
 		    else
 		    {
-			Vector<Integer> result = new Vector<Integer>();
-			while(m > 0)
-			{
-			    result.insertElementAt((m % 10), 0);
-			    m /= 10;
-			}
-			do
-			{
-			    result.insertElementAt(0, 0);
-			}while(result.size() < n.size());
-			for(Integer k : result)
-			{
-			    System.out.print(k);
-			}
+				Vector<Integer> result = new Vector<Integer>();
+				while(m > 0)
+				{
+				    result.insertElementAt((m % 10), 0);
+				    m /= 10;
+				}
+				do
+				{
+				    result.insertElementAt(0, 0);
+				}while(result.size() < n.size());
+				for(Integer k : result)
+				{
+				    System.out.print(k);
+				}
 		    }
 		}
     }

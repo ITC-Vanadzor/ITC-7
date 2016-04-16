77bbfd6a21265f95ea8ae9e0004d72f73f0d37ba
Ani121293 <ani_saghatelyan@mail.ru>
2016-04-16 19:50:04 +0400

    public functions  became private

--- Ani_Saghatelyan/Homework/Java/Kaprekar/Kaprekar.java
+++ Ani_Saghatelyan/Homework/Java/Kaprekar/Kaprekar.java
@@ -5,9 +5,7 @@ class Kaprekar {
 		System.out.print("Enter the number ");
 		int n = reader.nextInt();
 		Number tiv = new Number(n);
-		System.out.println("Min number is  "+ tiv.minNum());
-		System.out.println("Max number is  " + tiv.maxNum());
-		String format = "%0"+tiv.size()+"%n";
+		String format = "%0"+tiv.size()+"d\n";
 		System.out.printf(format,  tiv.Diff());
 	}
 }
--- Ani_Saghatelyan/Homework/Java/Kaprekar/Number.java
+++ Ani_Saghatelyan/Homework/Java/Kaprekar/Number.java
@@ -5,19 +5,16 @@ class Number {
 	private ArrayList<Integer> array = new ArrayList<Integer>();
 	public Number(int number) {
 		this.number = number;
-//The usage of this is not preferable either. You can see details in "Joshua_Bloch_-_Effective_Java_2nd_Edition" book.
-		this.toDigits();
+		toDigits();
 	}
-//The function is specific to this class as returns the size of the member array, so it should be declared as private.
     public int size()
     {
     	return array.size();
 
     }
 
-//public -> private ...
 
-	public void toDigits() {
+	private void toDigits() {
 		int temp = number;
                 while(temp > 0 ) {
                         array.add((Integer)temp%10);
@@ -26,15 +23,14 @@ class Number {
                 Collections.sort(array);
 
 	}
-// public -> private ... There is no requirement to print the min / max value
-	public int minNum() {
+	private  int minNum() {
 		int min=0;
 		for(int j=0;j<array.size();++j) {
 			min = min*10 + array.get(j);
 		}
 		return min;
 	}
-	public int maxNum() {
+	private  int maxNum() {
 		int max = 0;
 		for(int j=array.size()-1;j>=0;--j) {
 			max = max*10 + array.get(j);
@@ -42,8 +38,7 @@ class Number {
 		return max;
 	}
 	public int Diff() {
-		int diff = this.maxNum() - this.minNum();
-		 
+		int diff = maxNum() -minNum();
 		return diff;
 	}
 }

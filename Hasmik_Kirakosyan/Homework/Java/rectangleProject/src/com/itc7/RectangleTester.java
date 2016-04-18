818d8c4436ff50942d9f877ced6e326649e0a236
hasmikus@gmail.com <Hasmik>
2016-04-19 00:55:30 +0400

    Added annotations to Rectangle project

--- Hasmik_Kirakosyan/Homework/Java/rectangleProject/src/com/itc7/RectangleTester.java
+++ Hasmik_Kirakosyan/Homework/Java/rectangleProject/src/com/itc7/RectangleTester.java
@@ -1,5 +1,6 @@
 import com.itc7.utils.*;
 import com.itc7.entities.*;
+import java.lang.reflect.*;
 
 public class RectangleTester {
     public static void main (String[] args) {
@@ -9,5 +10,18 @@ public class RectangleTester {
         RectangleUtil util = new RectangleUtil(rectangle);
         System.out.println("Rectangle area : "+util.rectArea());
         System.out.println("Rectangle perimeter : "+util.rectPerimeter());
+        
+
+        try {
+            Method m = util.getClass( ).getMethod("rectArea");
+            Annotation[] annotations = m.getAnnotations();
+            System.out.println("All annotations:");
+            for(Annotation a : annotations)
+                System.out.println(a);
+        } catch (NoSuchMethodException exc) {
+            System.out.println("Method Not Found.");
+        }
     }
 }
+
+
--- Hasmik_Kirakosyan/Homework/Java/rectangleProject/src/com/itc7/utils/RectangleUtil.java
+++ Hasmik_Kirakosyan/Homework/Java/rectangleProject/src/com/itc7/utils/RectangleUtil.java
@@ -1,7 +1,22 @@
 package com.itc7.utils;
 import com.itc7.entities.*;
+import java.lang.reflect.*;
 
+@Retention(RetentionPolicy.RUNTIME)
+@interface Version {
+    public int version();
+    String author() default "Hasmik";
+} 
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Describe {
+    public int paramsNum();
+    public String methodName();
+}
+
+@Version(version = 1, author = "Another Author")
 public class RectangleUtil {
+
     private Rectangle rect;
     private int height;
     private int width;
@@ -20,10 +35,12 @@ public class RectangleUtil {
         width = Math.abs((rect.getRightCorner().getX() - rect.getLeftCorner().getX()));
     }
 
+    @Describe(paramsNum=0, methodName="rectArea")
     public int rectArea() {
         return width*height;
     }
 
+    @Describe(paramsNum=0, methodName="rectPeRImeter")
     public int rectPerimeter() {
         return 2*(width+height);
     }

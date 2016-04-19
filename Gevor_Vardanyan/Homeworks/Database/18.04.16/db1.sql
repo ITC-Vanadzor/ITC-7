7180fb7b9ecddd94a57b9158815632a446e8b1ac
GevorVardanyan <gevor.vardanyan.93.@mail.ru>
2016-04-19 11:45:38 +0400

    dbHDM

---
+++ Gevor_Vardanyan/Homeworks/Database/18.04.16/db1.sql
@@ -0,0 +1,12 @@
+Create database HDM;
+Use HDM;
+Create table user (id int not null auto_increment, username varchar(20) not null, email varchar(35) not null, password varchar(256) not null, primary key(id), key indexEmail (email));
+Create table field (field_id int not null auto_increment, opt_field varchar(55) not null, primary key(field_id));
+Create table account (opt_id int not null auto_increment, opt_value varchar(255) not null, u_id int not null, f_id int not null, primary key(opt_id), foreign key(u_id) references user(id), foreign key(f_id) references field(field_id));
+Create table receipt (id int not null auto_increment, time datetime not null, market varchar(50) not null, sum int not null, date date not null, comment text , u_id int not null, primary key(id), foreign key(u_id) references user(id), key indexTime (time, market));
+Insert into field(field_id, opt_field) values (1, 'surname'), (2, 'birthday'), (3, 'image');
+Create view userView AS select user.id as id, user.username as name, user.email as email, user.password as pass, field.opt_field as field, account.opt_value as val from ((account join user) join field) where ((account.u_id=user.id) and (account.f_id=field.field_id));
+
+-- Insert into user(id, username, email, password) values (1, 'Dorian', 'dy@.com', 'dorianyates');
+-- Insert into account(u_id, f_id, opt_value) values (1, 1, 'Yates'), (1, 2, '19.04.1962');
+-- Select * from userView;

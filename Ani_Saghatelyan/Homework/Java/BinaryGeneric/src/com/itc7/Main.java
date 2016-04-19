f6119b208fe97d20058e928c90bd1abab0d745d2
Ani121293 <ani_saghatelyan@mail.ru>
2016-04-20 01:14:33 +0400

    Added the programm about Qsearch using generics

--- Ani_Saghatelyan/Homework/Java/BinaryGeneric/src/com/itc7/Main.java
+++ Ani_Saghatelyan/Homework/Java/BinaryGeneric/src/com/itc7/Main.java
@@ -1,10 +1,12 @@
 package com.itc7;
-import binary.*;
+import com.itc7.binary.*;
 public  class Main
 {
 	 public  static  void main (String[] args)
 	 {
-         	Integer array[] = {1,3,4,5,6};
-         	System.out.println("Index of number is : "+ Binary.binarySearch(array, 0, 3, 5));
-	 }
+         	 Integer array[] = {1,3,4,5,6};
+		 Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+         	 System.out.println("Index of number is  for int : "+ Binary.binarySearch(array, 0, 5, 5));
+		 System.out.println("Index of number is for double : "+ Binary.binarySearch(doubleArray, 0, 5, 3.3));
+	}
 }

c4d4f4056a02611b5597003da42533c16f950e5b
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-04-20 21:54:43 +0400

    added binary search with collections sort

--- Edgar_Hakobyan/Homework/java/binarySearch/src/com/itc7/SearchTest.java
+++ Edgar_Hakobyan/Homework/java/binarySearch/src/com/itc7/SearchTest.java
@@ -1,15 +1,29 @@
 package com.itc7;
 
+import java.util.ArrayList;
+
 import com.itc7.binarySearch.BinarySearch;
 
 public class SearchTest {
 
-	public static void main(String[] args) {
-		BinarySearch obj = new BinarySearch();
-		Integer[] arr = {5,7,12,16,24,31};
-		System.out.println("index of value: " + obj.binarySearch(arr, 0, arr.length-1, 24));
-		
-		String[] str = {"aab", "dcg", "det", "dsd"};
-		System.out.println("index of value: " + obj.binarySearch(str, 0, str.length-1, "det"));
-	}
+    public static void main(String[] args) {
+        BinarySearch obj = new BinarySearch();
+        Integer[] arr1 = {5,7,12,16,24,31};
+        
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(16);
+        arr.add(7);
+        arr.add(31);
+        arr.add(5);
+        arr.add(12);
+        arr.add(24);
+        
+        
+        System.out.println(obj.compare(arr,arr1,0,arr1.length,7));
+        
+        //System.out.println("index of value with collections binarusearch: " + obj.sortByCollections(arr, 7));
+        
+        //System.out.println("index of value with binarysearch: " + obj.binarySearch(arr1, 0, arr1.length, 7));
+        
+    }
 }
--- Edgar_Hakobyan/Homework/java/binarySearch/src/com/itc7/binarySearch/BinarySearch.java
+++ Edgar_Hakobyan/Homework/java/binarySearch/src/com/itc7/binarySearch/BinarySearch.java
@@ -1,18 +1,30 @@
 package com.itc7.binarySearch;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BinarySearch {
-	public static <T extends Comparable<T>> int binarySearch(T[] array, int first, int last, T value){
-		if (first <= last) {
-			int mid = (first + last) / 2;
-			int cmp = array[mid].compareTo(value);
-			if(cmp < 0) {
-				return binarySearch(array, mid + 1, last, value);
-			}
-			if(cmp > 0) {
-				return binarySearch(array, first, mid - 1, value);
-			}
+    public static <T extends Number & Comparable<T>> int binarySearch(T[] array, int first, int last, T value){
+        if (first <= last) {
+            int mid = (first/2) + (last / 2);
+            int cmp = array[mid].compareTo(value);
+            if(cmp < 0) {
+                return binarySearch(array, mid + 1, last, value);
+            }
+            if(cmp > 0) {
+                return binarySearch(array, first, mid - 1, value);
+            }
             return mid;
-		}
-		return -1;
-	}
+        }
+        return -1;
+    }
+    
+    public static <T extends Number & Comparable<T>> int sortByCollections(ArrayList<T> list, T value) {
+        Collections.sort(list);
+        return Collections.binarySearch(list,value);  
+    }
+    
+    public <T extends Number & Comparable<T>> boolean compare(ArrayList<T> list, T[] array, int first, int last, T value) {
+        return sortByCollections(list,value) == binarySearch(array, first, last, value);
+    }
 }

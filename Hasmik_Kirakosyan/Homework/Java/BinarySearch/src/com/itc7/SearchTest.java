fe48e9960d639ae3e1998507fee69c5f13045919
hasmikus@gmail.com <Hasmik>
2016-04-20 16:23:13 +0400

    Added package and array.length to SearchTest

--- Hasmik_Kirakosyan/Homework/Java/BinarySearch/src/com/itc7/SearchTest.java
+++ Hasmik_Kirakosyan/Homework/Java/BinarySearch/src/com/itc7/SearchTest.java
@@ -1,9 +1,10 @@
+package com.itc7;
 import com.itc7.BinarySearch.*;
 
 public class SearchTest {
     public static void main (String [] args) {
         BinarySearch test = new BinarySearch();
-        Integer array[] = {1,6,5,9};
-        System.out.println("Needed number index is : "+test.binarySearch(array, 0, 3, 5));
+        Integer array[] =  {1, 3, 5, 9};
+        System.out.println("Needed number index is : "+test.binarySearch(array, 0, array.length, 5));
     }
 }

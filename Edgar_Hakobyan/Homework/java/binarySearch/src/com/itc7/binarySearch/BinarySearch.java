76102e0159c1c07b32a5ad3158a65b0adf897c96
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-04-20 13:36:25 +0400

    changed binarysearch

--- Edgar_Hakobyan/Homework/java/binarySearch/src/com/itc7/binarySearch/BinarySearch.java
+++ Edgar_Hakobyan/Homework/java/binarySearch/src/com/itc7/binarySearch/BinarySearch.java
@@ -2,18 +2,16 @@ package com.itc7.binarySearch;
 
 public class BinarySearch {
 	public static <T extends Comparable<T>> int binarySearch(T[] array, int first, int last, T value){
-		if (first < last) {
+		if (first <= last) {
 			int mid = (first + last) / 2;
 			int cmp = array[mid].compareTo(value);
-			if(array[mid] == value) {
-				return mid;
-			}
 			if(cmp < 0) {
 				return binarySearch(array, mid + 1, last, value);
 			}
 			if(cmp > 0) {
 				return binarySearch(array, first, mid - 1, value);
 			}
+            return mid;
 		}
 		return -1;
 	}

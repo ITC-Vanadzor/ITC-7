526db03810b9f26b2a7d8acdc3d152044462709b
hasmikus@gmail.com <Hasmik>
2016-04-24 21:56:54 +0400

    Added changed pom.xml and Test.java

--- Hasmik_Kirakosyan/Homework/Maven/testProject/App/src/main/java/com/itc7/apps/Test.java
+++ Hasmik_Kirakosyan/Homework/Maven/testProject/App/src/main/java/com/itc7/apps/Test.java
@@ -1,11 +1,12 @@
 package com.itc7.apps;
 import com.itc7.utils.App;
-
+//import java.util.Properties;
 public class Test
 {
     public static void main( String[] args )
     {
-        String filePath = args[0];
+        //Properties props = new Properties();
+        String filePath = System.getProperty("jsonFileName");
         System.out.println("Need key value is "+App.getKeyValue("Author", filePath));
     }
 }

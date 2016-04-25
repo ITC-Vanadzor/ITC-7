52f970845833416e3748cff0c357383b262cfb9d
hasmikus@gmail.com <Hasmik>
2016-04-26 01:05:23 +0400

    Made cleanup in Maven multimodule project

--- Hasmik_Kirakosyan/Homework/Maven/testProject/App/src/main/java/com/itc7/apps/Test.java
+++ Hasmik_Kirakosyan/Homework/Maven/testProject/App/src/main/java/com/itc7/apps/Test.java
@@ -1,11 +1,11 @@
 package com.itc7.apps;
 import com.itc7.utils.App;
-//import java.util.Properties;
+
+
 public class Test
 {
     public static void main( String[] args )
     {
-        //Properties props = new Properties();
         String filePath = System.getProperty("jsonFileName");
         System.out.println("Need key value is "+App.getKeyValue("Author", filePath));
     }

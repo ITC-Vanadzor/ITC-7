5a1f150f5605f8d6be28aa1ae3254c28cee35732
Ani121293 <ani_saghatelyan@mail.ru>
2016-04-26 01:59:05 +0400

    Added kaprekar with testng

--- Ani_Saghatelyan/Homework/TestNg/kaprekar/src/test/java/com/itc7/kaprekar/AppTest.java
+++ Ani_Saghatelyan/Homework/TestNg/kaprekar/src/test/java/com/itc7/kaprekar/AppTest.java
@@ -5,21 +5,72 @@ import java.util.ArrayList;
 public class AppTest 
 {    
    Number numObj =  new Number(495);
-   @Test (dependsOnMethods={"testToDigits"})
-  // positive test	
-   public  void testMinNum()
-   {
-	int result = numObj.minNum();
-        Assert.assertEquals(result, 459, "minNum() FUNCTION IS INVALID");
-   }
+    
+    @BeforeClass (groups = {"negative"})
+    public void testConstructorNeg()
+    {
+        Number obj=new Number((-5));
+        Assert.assertFalse(obj.number>0,"THE ARGUMENT OF CONSTRUCTOR IS LESS OR EQUAL THEN 0!!!");
+    }
 
-   @Test
-   public void testToDigits()
-   {
-	ArrayList <Integer> result =new ArrayList<Integer>();
-	result.add(4);
-	result.add(5);
-	result.add(9);
-   	Assert.assertTrue(result.equals(numObj.toDigits()), "toDigits() FUNCTION IS INVALID");
-   }
+    @BeforeClass  (groups = {"positive"})
+    public void testConstructorPos()
+    {
+        Number obj=new Number((10));
+        Assert.assertTrue(obj.number>0,"THE ARGUMENT OF CONSTRUCTOR IS LESS OR EQUAL THEN 0!!!");
+    }
+    @Test (dependsOnMethods={"testToDigits"}, groups = {"positive"})
+    public  void testMinNum()
+    {
+	    int result = numObj.minNum();
+        Assert.assertEquals(result, 459, "minNum() FUNCTION IS INVALID");
+    }
+
+    @Test (dependsOnMethods={"testToDigits"}  , groups = {"positive"})
+    public void testMaxNum()
+    {
+        int result=numObj.maxNum();
+        Assert.assertEquals(result, 954, "maxNum() FUNCTION IS INVALID");
+    }
+    
+    @Test (groups = {"negative"})
+    public void testMaxNumNeg()
+    {
+        int result=numObj.maxNum();
+        Assert.assertNotEquals(result, 459, "maxNum() FUNCTION IS INVALID negative");
+    }
+
+    @BeforeMethod
+    public void testToDigits()
+    {
+	    ArrayList <Integer> result =new ArrayList<Integer>();
+    	result.add(4);
+    	result.add(5);
+    	result.add(9);
+   	    Assert.assertTrue(result.equals(numObj.toDigits()), "toDigits() FUNCTION IS INVALID");
+    }
+
+    @Test (dependsOnMethods={"testMinNum"}, groups = {"positive"})
+    public void testDiff()
+    {
+         int result=numObj.Diff();
+        Assert.assertTrue(result==495, "DIFF() FUNCTION IS CORRECT");
+    }
+
+    @Test (dependsOnMethods={"testToDigits"}, groups = {"positive"})
+    public void testCount() 
+    {
+        int result=numObj.count();
+    	Assert.assertEquals(result,3, "count() FUNCTION IS INVALID");
+    }
+    
+    @Parameters("count")
+    @Test (dependsOnMethods={"testToDigits"}, groups = {"negative"})
+    public void testCountNeg(int param)
+    {
+        int result=numObj.count();
+    	Assert.assertNotEquals(result,param, "count() FUNCTION IS INVALID");
+    }
+
+
 }

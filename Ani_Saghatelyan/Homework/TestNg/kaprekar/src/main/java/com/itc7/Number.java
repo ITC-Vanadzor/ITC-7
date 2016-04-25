f2752380c2cb5021db97f13d23dab951677c4a8c
Smbat888 <ani_saghatelyan@mail.ru>
2016-04-25 17:40:27 +0400

    Added classwork for kaprekar testing with HAsmik

---
+++ Ani_Saghatelyan/Homework/TestNg/kaprekar/src/main/java/com/itc7/Number.java
@@ -0,0 +1,44 @@
+package com.itc7.kaprekar;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Number {
+	public  int number;
+	public ArrayList<Integer> array = new ArrayList<Integer>();
+	public Number(int number1) {
+		number = number1;
+		//toDigits();
+	}
+	public ArrayList<Integer>  toDigits() {
+		int temp = number;
+                while(temp > 0 ) {
+                        array.add((Integer)temp%10);
+                        temp = temp/10;
+                }
+                Collections.sort(array);
+		return array;
+	}
+	public int minNum() {
+		int min=0;
+		for(int j=0;j<array.size();++j) {
+			min = min*10 + array.get(j);
+		}
+		return min;
+	}
+	public int maxNum() {
+		int max = 0;
+		for(int j=array.size()-1;j>=0;--j) {
+			max = max*10 + array.get(j);
+		}
+		return max;
+	}
+	
+	public int count() {
+			  return array.size();
+	}
+	public int Diff() {
+		int diff = maxNum() - minNum();	
+		return diff;
+	}
+}
+
+

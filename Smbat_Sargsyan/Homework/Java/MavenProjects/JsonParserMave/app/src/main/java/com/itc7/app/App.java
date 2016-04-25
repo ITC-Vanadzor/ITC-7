44b61347eb6f646d87c21ce6742db7d0b35f402c
Smbat888 <Sargsyanyan@mail.ru>
2016-04-25 22:55:21 +0400

    added kaprekar solution testing project and json parser maven project

---
+++ Smbat_Sargsyan/Homework/Java/MavenProjects/JsonParserMave/app/src/main/java/com/itc7/app/App.java
@@ -0,0 +1,16 @@
+package com.itc7.app;
+
+/**
+ * Hello world!
+ *
+ */
+import com.itc7.utils.AppUtils;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from App!" );
+	AppUtils.print();
+	AppUtils.readJSON();
+    }
+}
---
+++ Smbat_Sargsyan/Homework/Java/MavenProjects/MavenKaprekar/kaprekar/src/main/java/com/itc7/Kaprekar.java
@@ -0,0 +1,13 @@
+package com.itc7;
+import java.util.Scanner;
+public class Kaprekar {
+	public static void main(String [] args) {
+		//Scanner reader = new Scanner(System.in);
+		//System.out.print("Enter the number ");
+		//int n = reader.nextInt();
+
+		//Number number = new Number(n);
+		System.out.print("Kaprekar number is  ");
+	//number.Diff();
+	}
+}

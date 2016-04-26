96a39f9b8cf57a5206a657837043187ae0234ac7
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-04-27 02:59:39 +0400

    added argument to path in multapp project

--- Edgar_Hakobyan/Homework/maven/multi-app/app/src/main/java/com/itc7/App.java
+++ Edgar_Hakobyan/Homework/maven/multi-app/app/src/main/java/com/itc7/App.java
@@ -1,15 +1,35 @@
 package com.itc7;
 
 import java.util.Scanner;
-import com.itc7.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        JsonExample.writeJson();
-        JsonExample.readJson(args[0],args[1]);
+        System.out.println("---------" + args[0]);
+        System.out.println("---------" + args[1]);
+        
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object obj = parser.parse(new FileReader(args[0]));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            System.out.println(jsonObject.get(args[1]));
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }

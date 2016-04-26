cc50475ca9b5db8604558270a2cabca8fb64d3ac
v-levonyan <v_levonyan@yahoo.com>
2016-04-26 17:42:08 +0400

    Changed  functions in programm

--- Ani_Saghatelyan/Homework/TestNg/kaprekar/src/main/java/com/itc7/App.java
+++ Ani_Saghatelyan/Homework/TestNg/kaprekar/src/main/java/com/itc7/App.java
@@ -1,13 +1,20 @@
 package com.itc7.kaprekar;
 import java.util.Scanner;
-
-class Kaprekar {
+import java.util.ArrayList;
+public class App {
 	public static void main(String [] args) {
 		Scanner reader = new Scanner(System.in);
 		System.out.print("Enter the number ");
 		int n = reader.nextInt();
-		Number tiv = new Number(n);
-		String format = "%0"+tiv.count()+"d\n";
-		System.out.format("\n Result "+format, tiv.Diff());
+		Number tiv = new Number();
+		tiv.setNumber(n);
+		ArrayList<Integer> array=new ArrayList<Integer>();
+		array=tiv.toDigits(n);
+		int min=tiv.minNum(array);
+		int max=tiv.maxNum(array);
+
+
+		String format = "%0"+tiv.count(array)+"d\n";
+		System.out.format("\n Result "+format, tiv.Diff(max,min));
 	}
 }
--- Ani_Saghatelyan/Homework/TestNg/kaprekar/src/main/java/com/itc7/Number.java
+++ Ani_Saghatelyan/Homework/TestNg/kaprekar/src/main/java/com/itc7/Number.java
@@ -2,14 +2,36 @@ package com.itc7.kaprekar;
 import java.util.ArrayList;
 import java.util.Collections;
 public class Number {
-	public  int number;
-	public ArrayList<Integer> array = new ArrayList<Integer>();
-	public Number(int number1) {
-		number = number1;
-		//toDigits();
+	private  int number;
+	public ArrayList<Integer> myArray = new ArrayList<Integer>();
+	
+	
+	
+	public int minNum(ArrayList<Integer> arr) {
+		int min=0;
+		for(int j=0;j<arr.size();++j) {
+			min = min*10 + arr.get(j);
+		}
+		return min;
 	}
-	public ArrayList<Integer>  toDigits() {
-		int temp = number;
+	public int maxNum(ArrayList<Integer> arr) {
+		int max = 0;
+		for(int j=arr.size()-1;j>=0;--j) {
+			max = max*10 + arr.get(j);
+		}
+		return max;
+	}
+    
+	public int count(ArrayList<Integer> arr) {
+			  return arr.size();
+	}
+	public int Diff(int max, int min) {
+		int diff = max - min;	
+		return diff;
+	}
+    public ArrayList<Integer>  toDigits(int num) {
+		ArrayList<Integer> array=new ArrayList<Integer>();
+		int temp = num;
                 while(temp > 0 ) {
                         array.add((Integer)temp%10);
                         temp = temp/10;
@@ -17,28 +39,9 @@ public class Number {
                 Collections.sort(array);
 		return array;
 	}
-	public int minNum() {
-		int min=0;
-		for(int j=0;j<array.size();++j) {
-			min = min*10 + array.get(j);
-		}
-		return min;
+    public  void setNumber(int num) 
+	{
+	    number = num;
 	}
-	public int maxNum() {
-		int max = 0;
-		for(int j=array.size()-1;j>=0;--j) {
-			max = max*10 + array.get(j);
-		}
-		return max;
-	}
-	
-	public int count() {
-			  return array.size();
-	}
-	public int Diff() {
-		int diff = maxNum() - minNum();	
-		return diff;
-	}
+
 }
-
-

26a0263a0318d3825fab884c7a481c985601a9ac
edgarhakobyan <edgar-hakobyan-1994@mail.ru>
2016-04-28 00:54:28 +0400

    added showmath

---
+++ Edgar_Hakobyan/Homework/jni/ShowMath.java
@@ -0,0 +1,9 @@
+public class  ShowMath {
+    public static void main(String[] args) {
+        MyMath math = new MyMath();
+        int[] arr = new int[]{1,4,2};
+        System.out.println(math.factorial(4));
+        System.out.println(math.getMax(arr);
+        System.out.println(math.getMin(arr);
+    }
+}

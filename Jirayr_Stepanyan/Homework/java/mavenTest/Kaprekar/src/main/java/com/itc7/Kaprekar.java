8e976eb50e0afb4795c3c687dac3cd295aeb2805
Jirayr <jiro-19944@mail.ru>
2016-04-27 21:23:05 +0400

    apdated kaprekar with Araksya

---
+++ Jirayr_Stepanyan/Homework/java/mavenTest/Kaprekar/src/main/java/com/itc7/Kaprekar.java
@@ -0,0 +1,17 @@
+package com.itc7;
+import java.util.ArrayList;
+import java.util.Scanner;
+class Kaprekar {
+    public static void main(String [] args) {
+	/*Scanner reader = new Scanner(System.in);
+	System.out.print("Enter the number ");
+	int n = reader.nextInt();*/
+	/*Number tiv = new Number();
+	tiv.setNumber(100);
+	ArrayList<Integer> tmpArray = new ArrayList<Integer>();
+	tmpArray = tiv.toDigits();
+	int max = tiv.maxNum(tmpArray);
+	int min = tiv.minNum();
+	System.out.println("Kaprekar number is " + tiv.diff(max, min, tmpArray.size()));*/
+    }
+}
---
+++ Jirayr_Stepanyan/Homework/java/mavenTest/Kaprekar/src/main/java/com/itc7/Number.java
@@ -0,0 +1,52 @@
+package com.itc7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Number {
+    public int number;
+    public ArrayList<Integer> digitsArray = new ArrayList<Integer>();
+   
+
+    public void setNumber(int number){
+	this.number=number;	
+    }
+
+    public ArrayList <Integer> toDigits() {
+	    int temp = this.number;
+	    while(temp > 0 ) {
+	        this.digitsArray.add((Integer)temp%10);
+	        temp = temp/10;
+	    }
+	    Collections.sort(this.digitsArray);
+	    return this.digitsArray;
+
+    }
+
+    public int minNum(ArrayList <Integer> array) {
+	    int min=0;
+	    for(int j=0;j<array.size();++j) {
+	        min = min*10 + array.get(j);
+	    }
+	    return min;
+    }
+
+    public int maxNum(ArrayList <Integer> array) {
+	    int max = 0;
+	    for(int j=array.size()-1;j>=0;--j) {
+	        max = max*10 + array.get(j);
+	    }
+	    return max;
+    }
+    
+    public String diff(int max, int min, int arraySize) {
+	    int diff = max - min;
+	    String result=String.format("%0" + arraySize + "d\n", diff);
+	    return result;
+	    
+    }
+}
+
+

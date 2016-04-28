f818b8680f122d841337564ae973979b5abc78b7
Araksya-Hambaryan <araksyahambaryan@mail.ru>
2016-04-28 22:56:01 +0400

    Update fileUtils.cpp

--- Araksya_Hambaryan/Homework/JNI/FileReader/fileUtils.cpp
+++ Araksya_Hambaryan/Homework/JNI/FileReader/fileUtils.cpp
@@ -19,7 +19,7 @@ class fileUtils{
 		std::cout << "File could not be created" << std::endl;
 	    }
 	}
-	void writeInFile(char* text) {
+	void writeInFile(std::string text) {
 	    myFile << text <<std::endl;
 	}
 	void readFile() {
